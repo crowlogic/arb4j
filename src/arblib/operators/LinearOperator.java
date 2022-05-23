1049142014f814f3010731c96f5f80b13dbfb657
Stephen Crowley <stephencrowley214@gmail.com>
2022-05-22 20:32:04 -0500

    package arblib.operators;
    /**
     * A an operator is a linear operator if it transforms the space it
    operates on
     * in the following way:
     * <ul>
     * <li>f(x+y)=f(x)+f(y) ∀x & ∀y</li>
     * <li>f(cx)=cf(x) ∀x & ∀ constant scalars c</li>
     * </ul>
     *
     * TODO: define the interfaces of the space it operates on as generic
    arguments
     */
    public interface LinearOperator
    {
    
    }

---
+++ src/arblib/operators/LinearOperator.java
@@ -0,0 +1,16 @@
+package arblib.operators;
+
+/**
+ * A an operator is a linear operator if it transforms the space it operates on
+ * in the following way:
+ * <ul>
+ * <li>f(x+y)=f(x)+f(y) ∀x & ∀y</li>
+ * <li>f(cx)=cf(x) ∀x & ∀ constant scalars c</li>
+ * </ul>
+ * 
+ * TODO: define the interfaces of the space it operates on as generic arguments
+ */
+public interface LinearOperator
+{
+
+}
