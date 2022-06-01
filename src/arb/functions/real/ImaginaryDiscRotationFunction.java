d97a2937d5c3be84f256ab7978ed13d06128e7aa
Stephen <stephencrowley214@gmail.com>
2022-05-31 21:34:05 -0500

    the functions ReIm(f(t+h*e^(i*a)))

---
+++ src/arb/functions/real/ImaginaryDiscRotationFunction.java
@@ -0,0 +1,15 @@
+package arb.functions.real;
+
+import arb.*;
+
+public class ImaginaryDiscRotationFunction implements
+                                           RealFunction
+{
+
+  @Override
+  public Real evaluate(Real z, int order, int prec, Real res)
+  {
+    throw new UnsupportedOperationException("TODO");
+  }
+
+}
