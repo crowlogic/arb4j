4a86cb40fd9bfe4c610bcb85a09ff9d3a11fa8fd
Stephen <stephencrowley214@gmail.com>
2022-06-06 16:39:17 -0500

    Merge branch 'master' of git@github.com:crowlogic/arb4j.git

--- src/arb/viz/rendering/SRenderer.java
+++ src/arb/viz/rendering/SRenderer.java
@@ -1,43 +1,48 @@
 package arb.viz.rendering;
 
-import java.awt.Dimension;
-import java.awt.geom.NoninvertibleTransformException;
-import java.awt.geom.Rectangle2D;
+import java.awt.*;
+import java.awt.geom.*;
 import java.awt.geom.Rectangle2D.Double;
-import java.io.IOException;
+import java.io.*;
 
-import arb.functions.complex.SFunction;
-import arb.viz.ComplexFunctionRenderer;
-import arb.viz.Part;
+import arb.*;
+import arb.functions.complex.*;
+import arb.viz.*;
 
 public class SRenderer extends
-                      ComplexFunctionRenderer<SFunction>
+                       ComplexFunctionRenderer<SFunction>
 {
 
-  public SRenderer(Dimension screen, Double domain) throws NoninvertibleTransformException
+  private static final Double    domain     = new Rectangle2D.Double(-Math.PI * 2,
+                                                                     -1.5 * 2,
+                                                                     Math.PI * 2 * 2,
+                                                                     3 * 2);
+  private static final Dimension dimensions = new Dimension(2000 / 2,
+                                                            1000 / 2);
+
+  public SRenderer(Dimension screen, Double domain, Real scale) throws NoninvertibleTransformException
   {
     super(screen,
           domain,
-          new SFunction());
+          new SFunction(scale));
   }
 
   public SRenderer() throws NoninvertibleTransformException
   {
-    this(new Dimension(2000 / 2,
-                       1000 / 2),
-         new Rectangle2D.Double(-Math.PI * 2,
-                                -1.5 * 2,
-                                Math.PI * 2 * 2,
-                                3 * 2));
+    this(dimensions,
+         domain,
+         Constants.ONE);
   }
 
   @SuppressWarnings("resource")
   public static void main(String args[]) throws IOException, NoninvertibleTransformException, InterruptedException
   {
 
-    SRenderer plotter = new SRenderer();
+    SRenderer plotter = new SRenderer(dimensions,
+                                      domain,
+                                      new Real().set(3));
 
-    plotter.color_mode  = 1;
+    plotter.color_mode  = 0;
     plotter.displayMode = Part.Blend;
     plotter.render();
 
