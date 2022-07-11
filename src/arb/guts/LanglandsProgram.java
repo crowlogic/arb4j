5b72e8b9eddf98905ac38eadef186c8d89325395
Stephen <stephencrowley214@gmail.com>
2022-07-11 04:33:04 -0500

    ...

--- src/arb/guts/LanglandsProgram.java
+++ src/arb/guts/LanglandsProgram.java
@@ -3,35 +3,33 @@ package arb.guts;
 import arb.*;
 
 /**
- * <h1>'The' Langlands program</h1>
+ * The Langlands program is a grand unified theory of mathematics which includes
+ * the search for {@link LanglandsReciprocity} which is a generalization of
+ * {@link ArtinReciprocity} to non-Abelian Galois extensions of
+ * {@link NumberField}s<br>
  * 
- * <code>
-is a grand unified theory of mathematics which includes the search for {@link LanglandsReciprocity} 
-which is a generalization of {@link ArtinReciprocity} to non-Abelian Galois extensions of 
-number fields. 
-
-In a January 1967 letter to André Weil, Langlands proposed that the mathematics 
-of algebra (Galois representations) and analysis (automorphic forms) are intimately related, 
-and that congruences over finite fields are related to infinite-dimensional representation theory. 
-
-In particular, Langlands conjectured that the transformations behind general reciprocity laws 
-could be represented by means of matrices such as {@link RealMatrix} and {@link ComplexMatrix}
-
-In 1998, three mathematicians proved Langlands' conjectures for local fields, and in a November 1999 
-lecture at the Institute for Advanced Study at Princeton University, L. Lafforgue presented a proof 
-of the conjectures for function fields. This leaves only the case of {@link NumberField}s unresolved.
-
-&#64;see https://mathworld.wolfram.com/LanglandsProgram.html
-&#64;see https://en.wikipedia.org/wiki/Langlands_program
-@see https://www.ias.edu/ideas/modern-mathematics-and-langlands-program 
- 
-See also
-{@link ArtinReciprocity}
-Automorphic Form
-{@link LanglandsReciprocity}
-{@link Reciprocity} Theorem
-Taniyama-Shimura Conjecture
- * </code>
+ * Langlands proposed that the mathematics of algebra of Galois representations
+ * and analysis of automorphic forms are intimately related, and that
+ * congruences over finite fields are related to infinite-dimensional
+ * representation theory.<br.
+ * 
+ * In particular, Langlands particularly conjectured that the transformations
+ * behind general {@link Reciprocity} laws could be represented by means of
+ * matrices (such as {@link RealMatrix} and {@link ComplexMatrix} )<br>
+ * 
+ * In 1998, three mathematicians proved Langlands' conjectures for local fields,
+ * and in a November 1999 lecture at the Institute for Advanced Study at
+ * Princeton University, L. Lafforgue presented a proof of the conjectures for
+ * function fields. This leaves only the case of {@link NumberField}s
+ * unresolved.<br>
+ * 
+ * https://mathworld.wolfram.com/LanglandsProgram.html
+ * https://en.wikipedia.org/wiki/Langlands_program
+ * https://www.ias.edu/ideas/modern-mathematics-and-langlands-program
+ * 
+ * @see {@link ArtinReciprocity} as well as Automorphic Forms
+ * @see {@link LanglandsReciprocity}
+ * @see {@link Reciprocity} and the Taniyama-Shimura Conjecture
  */
 public interface LanglandsProgram
 {
--- src/arb/guts/LanglandsReciprocity.java
+++ src/arb/guts/LanglandsReciprocity.java
@@ -1,7 +1,9 @@
 package arb.guts;
 
 /**
+
  * @see LanglandsProgram
+ * @see http://www.math.uchicago.edu/~emerton/pdffiles/reciprocity.pdf
  */
 public interface LanglandsReciprocity extends
                                       ArtinReciprocity
