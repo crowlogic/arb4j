cf468005e9e3d7fe0a5a96a1855245d62d105bdd
Ωω <@.>
2022-07-17 18:25:30 -0500

    add renderer for ξ function and     assert order == 1 : "TODO: implement derivative d/dx(ξ(x)) = 1/2*π^(-t/2)*Γ[1 + t]*((2 + Log[π -t*Log[π] + (t - 1)*PolyGamma[0, 1 + t/2])*ζ[t] + 2(t-1)*ζ'[t])";

--- src/arb/functions/complex/RiemannξFunction.java
+++ src/arb/functions/complex/RiemannξFunction.java
@@ -12,7 +12,7 @@ public class RiemannξFunction implements
   public Complex evaluate(Complex t, int order, int prec, Complex res)
   {
     order = max(0, 1);
-    assert order == 1 : "TODO: implement derivatives of ξ";
+    assert order == 1 : "TODO: implement derivative d/dx(ξ(x)) = 1/2*π^(-t/2)*Γ[1 + t]*((2 + Log[π -t*Log[π] + (t - 1)*PolyGamma[0, 1 + t/2])*ζ[t] + 2(t-1)*ζ'[t])";
     arb.arb.acb_dirichlet_xi(res, t, prec);
     return res;
   }
---
+++ src/arb/viz/rendering/ξRenderer.java
@@ -0,0 +1,49 @@
+package arb.viz.rendering;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.*;
+
+import arb.*;
+import arb.functions.complex.*;
+import arb.functions.complex.dynamics.*;
+import arb.viz.*;
+
+/**
+ * Renders the {@link YFunction}
+ */
+public class ξRenderer extends
+                       ComplexFunctionRenderer<RiemannξFunction>
+{
+  @SuppressWarnings("resource")
+  public static void main(String args[]) throws NoninvertibleTransformException, IOException
+  {
+    ξRenderer renderer = new ξRenderer();
+    renderer.render();
+  }
+
+  public static final int width  = 1900 / 2;
+  public static final int height = 950 / 2;
+
+  public ξRenderer(Real vscale) throws NoninvertibleTransformException
+  {
+    super(new Dimension(width,
+                        height),
+          new Rectangle2D.Double(-20,
+                                 -30,
+                                 40,
+                                 120),
+          new RiemannξFunction());
+
+    colorMode   = 5;
+    displayMode = Part.Real;
+
+  }
+
+  public ξRenderer() throws NoninvertibleTransformException
+  {
+    this(new Real("3",
+                  128));
+  }
+
+}
