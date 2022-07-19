ab17e2c3f30be799ea1224bc33023397f08744d4
Ωω <@.>
2022-07-18 23:41:48 -0500

    2do

--- src/arb/expression/Function.java
+++ src/arb/expression/Function.java
@@ -1,5 +1,10 @@
 package arb.expression;
 
+/**
+ * TODO: harmonize this with {@link arb.functions.Function} and its descendants
+ * 
+ * @param <T>
+ */
 @FunctionalInterface
 public interface Function<T>
 {
