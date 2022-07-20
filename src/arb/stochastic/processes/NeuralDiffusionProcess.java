bf3a0659d250cd981174e43041ba36804fa94758
Ωω <@.>
2022-07-19 23:19:51 -0500

    /**  * TODO: implement  *  * @see <a href="doc-files/NeuralDiffusionProcess.pdf">Neural Diffusion Processes</a>  */ public abstract class NeuralDiffusionProcess implements                                              DiffusionProcess<Real, Real> {
    }

---
+++ src/arb/stochastic/processes/NeuralDiffusionProcess.java
@@ -0,0 +1,16 @@
+package arb.stochastic.processes;
+
+import arb.Real;
+import arb.functions.Function;
+import arb.stochastic.*;
+
+/**
+ * TODO: implement
+ * 
+ * @see <a href="doc-files/NeuralDiffusionProcess.pdf">Neural Diffusion Processes</a>
+ */
+public abstract class NeuralDiffusionProcess implements
+                                             DiffusionProcess<Real, Real>
+{
+
+}
