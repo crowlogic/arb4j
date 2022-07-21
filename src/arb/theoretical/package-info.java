b57ae103858640e10943a3870883d6a7c9e9eb5c
Ωω <@.>
2022-07-21 17:07:59 -0500

    /**  * A {@link arb.theoretical.Theory} is an insight into the natural world; a  * concept that is not yet verified but that if true would explain certain facts  * or phenomena;  */

---
+++ src/arb/theoretical/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * A {@link arb.theoretical.Theory} is an insight into the natural world; a
+ * concept that is not yet verified but that if true would explain certain facts
+ * or phenomena;
+ */
+package arb.theoretical;
