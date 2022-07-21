44f8b8b4a340bb3dc3ce4742e3263ad423d960a0
Ωω <@.>
2022-07-21 16:19:47 -0500

    conceptual stuff

--- src/arb/guts/mathematical/physics/YangMillsTheory.java
+++ src/arb/guts/mathematical/physics/YangMillsTheory.java
@@ -1,5 +1,18 @@
 package arb.guts.mathematical.physics;
 
+/**
+ * Yang–Mills theory is a {@link GaugeTheory} based on a special unitary group
+ * SU(N), or more generally any compact, reductive Lie algebra. Yang–Mills
+ * theory seeks to describe the behavior of <b>elementary particles</b> using these
+ * non-commutative Lie groups and is at the core of the unification of the
+ * electromagnetic force and weak forces (i.e. U(1) × SU(2)) as well as quantum
+ * chromodynamics, the theory of the strong force (based on SU(3)). Thus it
+ * forms the basis of our understanding of the Standard Model of particle
+ * physics.
+ * 
+ * @see <a href=
+ *      "https://en.wikipedia.org/wiki/Yang%E2%80%93Mills_theory">Wikipedia</a>
+ */
 public interface YangMillsTheory extends
                                  GaugeTheory
 {
