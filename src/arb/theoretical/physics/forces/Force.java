ef4717b1cd0407fbbe4d2064f890da1665b0d504
Ωω <@.>
2022-07-22 00:56:12 -0500

    its a start

--- src/arb/theoretical/physics/forces/Force.java
+++ src/arb/theoretical/physics/forces/Force.java
@@ -1,5 +1,9 @@
 package arb.theoretical.physics.forces;
 
+/**
+ * the influence that produces a change in a physical quantity; "force equals
+ * mass times acceleration"
+ */
 public interface Force
 {
 
