b8b4a2be200e323bf64df5d1677ab3e7455d1146
Stephen <stephencrowley214@gmail.com>
2022-07-24 17:17:14 -0500

    entire funcs

---
+++ src/arb/domains/ComplexPlane.java
@@ -0,0 +1,16 @@
+package arb.domains;
+
+import arb.*;
+import arb.spaces.*;
+
+/**
+ * The complex plane is the plane formed by the {@link Complex} numbers, with a
+ * Cartesian coordinate system such that the x-axis, called real axis, is formed
+ * by the {@link Real} numbers, and the y-axis, called imaginary axis, is formed
+ * by the imaginary numbers.
+ */
+public interface ComplexPlane extends
+                              Space<Complex>
+{
+
+}
---
+++ src/arb/functions/complex/EntireFunction.java
@@ -0,0 +1,11 @@
+package arb.functions.complex;
+
+/**
+ * an entire function, also called an integral function, is a complex-valued
+ * function that is holomorphic on the whole complex plane.
+ */
+public interface EntireFunction extends
+                                HolomorphicFunction
+{
+
+}
