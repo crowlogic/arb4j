b759aea256c7be050804f48aa9741ca595548e46
Stephen <stephencrowley214@gmail.com>
2022-07-24 01:50:53 -0500

    establishing the arb.format package

---
+++ src/arb/format/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * 79 Moby Thesaurus words for "format": Platonic form, Platonic idea, aesthetic
+ * form, anatomy, appearance, archetype, architectonics, architecture,
+ * arrangement, art form, aspect, build, building, cast, composition,
+ * configuration, conformation, constitution, construction, contents, creation,
+ * cut, design, dimensions, fabric, fabrication, fashion, fashioning,
+ * figuration, figure, forging, form, formation, frame, genre, getup,
+ * impression, inner form, layout, looks, make, makeup, making, manufacture,
+ * matrix, modality, mode, model, mold, molding, order, organic structure,
+ * organism, organization, pattern, patterning, physique, plan, production,
+ * prototype, set, set-up, setup, shape, shaping, significant form, size, stamp,
+ * structure, structuring, style, tectonics, texture, tissue, turn, type, warp
+ * and woof, weave, web
+ */
+package arb.format;
