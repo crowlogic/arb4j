57fde1b198338524e6c34914c1858195a1df8c2d
Ωω <@.>
2022-07-25 00:25:13 -0500

    define

--- src/arb/biology/Substance.java
+++ src/arb/biology/Substance.java
@@ -1,5 +1,31 @@
 package arb.biology;
 
+/**
+ * A chemical substance is a form of matter having constant chemical composition
+ * and characteristic properties.[1][2] Some references add that chemical
+ * substance cannot be separated into its constituent elements by physical
+ * separation methods, i.e., without breaking chemical bonds.[3] Chemical
+ * substances can be simple substances,[4] chemical compounds, or alloys.
+ * Chemical elements may or may not be included in the definition, depending on
+ * expert viewpoint.[citation needed]
+ * 
+ * Chemical substances are often called 'pure' to set them apart from mixtures.
+ * A common example of a chemical substance is pure water; it has the same
+ * properties and the same ratio of hydrogen to oxygen whether it is isolated
+ * from a river or made in a laboratory. Other chemical substances commonly
+ * encountered in pure form are diamond (carbon), gold, table salt (sodium
+ * chloride) and refined sugar (sucrose). However, in practice, no substance is
+ * entirely pure, and chemical purity is specified according to the intended use
+ * of the chemical.
+ * 
+ * Chemical substances exist as solids, liquids, gases, or plasma, and may
+ * change between these phases of matter with changes in temperature or pressure
+ * and time. Chemical substances may be combined or converted to others by means
+ * of {@link ChemicalReaction}s.
+ * 
+ * @see <a href=
+ *      "https://en.wikipedia.org/wiki/Chemical_substance">Wikipedia</Wikipedia>
+ */
 public interface Substance
 {
 
