0343a4ea6b5759bc1a0362b0a2fd3cd33daf754c
Stephen <stephencrowley214@gmail.com>
2022-07-25 15:31:54 -0500

    diffgeo

---
+++ src/arb/differential/geometry/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Differential geometry is a mathematical discipline that studies the geometry
+ * of smooth shapes and smooth spaces, otherwise known as smooth
+ * {@link arb.geometry.Manifold}s. It uses the techniques of differential
+ * calculus, integral calculus, linear algebra and multilinear algebra. The
+ * field has its origins in the study of spherical geometry as far back as
+ * antiquity. It also relates to astronomy, the geodesy of the Earth, and later
+ * the study of hyperbolic geometry by Lobachevsky. The simplest examples of
+ * smooth {@link arb.spaces.Space}s are the plane and space
+ * {@link arb.curves.Curve} and {@link arb.surfaces.Surface} in the
+ * three-dimensional {@link arb.spaces.EuclideanSpace}, and the study of these
+ * shapes formed the basis for development of modern differential geometry
+ * during the 18th and 19th centuries.
+ */
+package arb.differential.geometry;
