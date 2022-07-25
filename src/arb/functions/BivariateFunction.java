c9f96faabbdd3eb5697f0d9e3c27a01fc78e3daf
Ωω <@.>
2022-07-25 00:55:26 -0500

    simplify

--- src/arb/functions/BivariateFunction.java
+++ src/arb/functions/BivariateFunction.java
@@ -3,13 +3,7 @@ package arb.functions;
 import arb.OrderedPair;
 
 public interface BivariateFunction<Ω, X> extends
-                                  MultivariateFunction<OrderedPair<Ω,Ω>, X>
+                                  Function<OrderedPair<Ω, Ω>, X>
 {
-
-  @Override
-  default int argDim()
-  {
-    return 2;
-  }
 
 }
