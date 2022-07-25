d263eb95c1161cb4d2372aa6ac205342bbee7282
Stephen <stephencrowley214@gmail.com>
2022-07-25 15:54:30 -0500

    Merge branch 'master' of ssh://bb/git/arb4j

---
+++ src/arb/curves/UnitSpeedCurve.java
@@ -0,0 +1,10 @@
+package arb.curves;
+
+/**
+ * A {@link Curve} that has been normalized so that its length is equal to 1
+ */
+public interface UnitSpeedCurve extends
+                                Curve
+{
+
+}
