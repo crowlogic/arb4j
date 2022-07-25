a3acb4e9e096a714014466eb562a7ac95ed4ed97
Ωω <@.>
2022-07-25 17:26:25 -0500

    the desingularized Newton flow naturally extends from the Newton flow

--- src/arb/functions/complex/dynamics/DesingularizedNewtonFlow.java
+++ src/arb/functions/complex/dynamics/DesingularizedNewtonFlow.java
@@ -1,11 +1,11 @@
 package arb.functions.complex.dynamics;
 
 import arb.Complex;
-import arb.exceptions.NotDifferentiableException;
-import arb.functions.complex.*;
+import arb.functions.complex.EntireFunction;
+import arb.functions.complex.MeromorphicFunction;
 
 /**
- * <code>
+ * The desingularized <code>
  * 
  *    -f(t)*conjugate(f'(t))
  * ---------------------------
@@ -13,7 +13,8 @@ import arb.functions.complex.*;
  *   
  * </code>
  */
-public class DesingularizedNewtonFlow<F extends MeromorphicFunction> implements
+public class DesingularizedNewtonFlow<F extends MeromorphicFunction> extends
+                                     NewtonFlow implements
                                      EntireFunction
 {
 
@@ -26,28 +27,8 @@ public class DesingularizedNewtonFlow<F extends MeromorphicFunction> implements
 
   public DesingularizedNewtonFlow(F f)
   {
-    this.f = f;
+    super(f);
   }
-
-  public F                  f;
-
-  final HolomorphicFunction diff = (t, order, prec, w) ->
-                                 {
-                                   try ( Complex s = Complex.newVector(3);)
-                                   {
-                                     f.evaluate(t, 3, prec, s);
-                                     Complex a = s.mul(s.get(2), prec, w);
-                                     Complex b = s.get(1).pow(2, prec, s.get(0)).conj();
-                                     a.mul(b, prec, w);
-                                     if (!w.isFinite())
-                                     {
-                                       w.getImag().zero();
-                                       w.getReal().one().div(f.multiplicityOfRoot(t), prec);
-                                     }
-                                     w.neg(w).add(1, prec, w);
-                                     return w;
-                                   }
-                                 };
 
   @Override
   public Complex evaluate(Complex z, int order, int prec, Complex w)
@@ -70,12 +51,4 @@ public class DesingularizedNewtonFlow<F extends MeromorphicFunction> implements
     return w;
   }
 
-  /**
-   * @return (f(t)*f''(t))/(f'(t)^2)
-   */
-  @Override
-  public HolomorphicFunction differential() throws NotDifferentiableException
-  {
-    return diff;
-  }
 }
