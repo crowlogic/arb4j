0da696982ea28c11583c7f2ecb0f6f1f14aa146b
Ωω <@.>
2022-07-26 16:27:33 -0500

    class to hold the grid for integration/discretization

---
+++ src/arb/PartitionedFloatInterval.java
@@ -0,0 +1,12 @@
+package arb;
+
+public class PartitionedFloatInterval 
+{
+  FloatInterval interval;
+  Float partitions;
+  
+  public PartitionedFloatInterval( FloatInterval interval, int n )
+  {
+    assert false : "allocate a chunk of memory and then use Cptr offsets for fast access";   
+  }
+}
