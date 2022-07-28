ba2260f91c3aeb08fd17b74d3c0c5f07cce76a5c
Ωω <@.>
2022-07-27 22:51:02 -0500

    set domain and render rectangle to be equal proportion so that the aspect ratio is 1:1

--- src/arb/viz/rendering/YRenderer.java
+++ src/arb/viz/rendering/YRenderer.java
@@ -21,20 +21,21 @@ public class YRenderer extends
   public static void main(String args[]) throws NoninvertibleTransformException, IOException
   {
     YRenderer renderer = new YRenderer();
+    renderer.displayMode = Part.Phase;
     renderer.render();
   }
 
-  public static final int width  = 1900;
-  public static final int height = 950;
+  public static final int height = 1000;
+  public static final int width  = height*2;
 
   public YRenderer(Real vscale) throws NoninvertibleTransformException
   {
     super(new Dimension(width,
                         height),
           new Rectangle2D.Double(-10,
-                                 -22,
-                                 45,
-                                 44),
+                                 -24,
+                                 48*2,
+                                 48),
           new NewtonFlow<YFunction>(new YFunction(vscale)));
 
     colorMode   = 4;
