48a84b2248478b9dc2004d9e7021b6613fac6a44
Ωω <@.>
2022-07-29 15:56:11 -0500

    add TODO.txt since i fucking hate web interfaces

--- src/arb/stochastic/processes/StochasticEulerIntegratorTest.java
+++ src/arb/stochastic/processes/StochasticEulerIntegratorTest.java
@@ -1,5 +1,8 @@
 package arb.stochastic.processes;
 
+import static arb.RealConstants.one;
+import static arb.utensils.Utilities.println;
+
 import arb.*;
 import junit.framework.TestCase;
 
@@ -9,16 +12,15 @@ public class StochasticEulerIntegratorTest extends
   public static final int prec = 256;
   private Real            σ;
 
-  public void testIntegrator()
+  public void testWienerProcessIntegration()
   {
     RandomState               randomState = new RandomState((int) (Math.random() * Integer.MAX_VALUE));
-    GaussianProcess           B           = new WienerProcess(new Real("2.3",
-                                                                       128));
+    GaussianProcess           B           = new WienerProcess(one);
     StochasticEulerIntegrator integrator  = new StochasticEulerIntegrator(B,
                                                                           randomState);
     FloatInterval             interval    = new FloatInterval(0,
-                                                              100);
-    int                       n           = 50 * 1000;
+                                                              10);
+    int                       n           = 100 * 1000;
 
     DiffusionProcessState     state       = new DiffusionProcessState();
     EvaluationSequence        samplePath  = integrator.integrate(interval, prec, n, state);
@@ -28,20 +30,7 @@ public class StochasticEulerIntegratorTest extends
     System.out.println("     μ=" + μ);
     double absMean = Math.abs(μ.doubleValue());
 
-    assertTrue(absMean + " is too far away from zero with seed=" + randomState.getInitialValue(), absMean < 0.1);
-
-    samplePath.values.stream().map(x -> x.pow(2, prec));
-
-    Real σ = samplePath.values.arithmeticMean(prec, new Real()).abs().pow(2,prec);
-
-    σ.printPrecision = true;
-    double σdiff = Math.abs(σ.sub(samplePath.partition.δt, prec, new Real()).doubleValue());
-
-    System.out.println("     dt=" + samplePath.partition.δt.toString(75));
-    System.out.println("      σ=" + σ.toString(75) + "\nseed=" + randomState.getInitialValue() + "\nΔσ=" + σdiff);
-    assertTrue(samplePath.partition.δt + " is too far away from σ=" + σ + " with seed="
-                  + randomState.getInitialValue(),
-               σdiff < 0.001);
+    assert absMean < 0.03 : absMean + " is too far away from zero with seed=" + randomState.getInitialValue();
 
     /**
      * TODO: add Real.stdev() and Real.variance() methods
