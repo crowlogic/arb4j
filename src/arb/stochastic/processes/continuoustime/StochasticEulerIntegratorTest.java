7eb056f163e8dfca2e671612421080c1d7d769c8
Ωω <@.>
2022-07-29 17:40:58 -0500

    Merge branch 'master' of git@github.com:crowlogic/arb4j.git

--- src/arb/stochastic/processes/continuoustime/StochasticEulerIntegratorTest.java
+++ src/arb/stochastic/processes/continuoustime/StochasticEulerIntegratorTest.java
@@ -14,23 +14,25 @@ public class StochasticEulerIntegratorTest extends
 
   public void testWienerProcessIntegration()
   {
-    RandomState               randomState = new RandomState((int) (Math.random() * Integer.MAX_VALUE));
-    GaussianProcess           B           = new WienerProcess(one);
-    StochasticEulerIntegrator integrator  = new StochasticEulerIntegrator(B,
-                                                                          randomState);
-    FloatInterval             interval    = new FloatInterval(0,
-                                                              10);
-    int                       n           = 100 * 1000;
+    RandomState     randomState = new RandomState((int) (Math.random() * Integer.MAX_VALUE));
+    GaussianProcess B           = new WienerProcess(one);
+    try ( StochasticEulerIntegrator integrator = new StochasticEulerIntegrator(B,
+                                                                               randomState))
+    {
+      FloatInterval         interval   = new FloatInterval(0,
+                                                           10);
+      int                   n          = 100 * 1000;
 
-    DiffusionProcessState     state       = new DiffusionProcessState();
-    EvaluationSequence        samplePath  = integrator.integrate(interval, prec, n, state);
+      DiffusionProcessState state      = new DiffusionProcessState();
+      EvaluationSequence    samplePath = integrator.integrate(interval, prec, n, state);
 
-    Real                      μ           = samplePath.values.arithmeticMean(prec, new Real());
-    μ.printPrecision = true;
-    System.out.println("     μ=" + μ);
-    double absMean = Math.abs(μ.doubleValue());
+      Real                  μ          = samplePath.values.arithmeticMean(prec, new Real());
+      μ.printPrecision = true;
+      System.out.println("     μ=" + μ);
+      double absMean = Math.abs(μ.doubleValue());
 
-    assert absMean < 0.03 : absMean + " is too far away from zero with seed=" + randomState.getInitialValue();
+      assert absMean < 0.03 : absMean + " is too far away from zero with seed=" + randomState.getInitialValue();
+    }
 
     /**
      * TODO: add Real.stdev() and Real.variance() methods
