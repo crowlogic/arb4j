638383b1c4c6787d1b33812d147226a1dbc06533
Ωω <@.>
2022-07-31 18:12:58 -0500

    ...

--- src/arb/stochastic/InverseStandardGaussianDistributionFunction.java
+++ src/arb/stochastic/InverseStandardGaussianDistributionFunction.java
@@ -1,6 +1,6 @@
 package arb.stochastic;
 
-import static arb.RealConstants.one;
+import static arb.RealConstants.*;
 
 import arb.*;
 import arb.functions.real.RealFunction;
@@ -15,7 +15,7 @@ public class InverseStandardGaussianDistributionFunction implements
     order = Math.max(0, order);
     assert order == 1 : "TODO: only 1st order implemented (for now)";
     arb.arb_hypgeom_erfinv(res, x.mul(2, prec, res).sub(one, prec, res), prec);
-    res.mul(RealConstants.sqrt2, prec, res);
+    res.mul(sqrt2, prec, res);
     return res;
   }
 
