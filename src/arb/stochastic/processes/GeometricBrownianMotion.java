9095d29f28774a3104705cca629f693d2efbc1f2
Ωω <@.>
2022-08-01 01:19:47 -0500

    i swear there was a giant fucking Wiener on wheels in Oklahoma. I wonder how much the pork industry pays for that

---
+++ src/arb/stochastic/processes/GeometricBrownianMotion.java
@@ -0,0 +1,26 @@
+package arb.stochastic.processes;
+
+/**
+ * TODO
+ */
+public class GeometricBrownianMotion implements
+                                     DiffusionProcess
+{
+
+  @Override
+  public DriftCoeffecientFunction μ()
+  {
+    assert false : "implement me";
+    return null;
+    
+  }
+
+  @Override
+  public DiffusionCoeffecientFunction σ()
+  {
+    assert false : "implement me";
+    return null;
+    
+  }
+
+}
