a762003182cd08ac5d0ec23796fa8fdf0d3ba403
Ωω <@.>
2022-08-01 18:42:03 -0500

    first pass

--- src/arb/stochastic/processes/GeometricBrownianMotion.java
+++ src/arb/stochastic/processes/GeometricBrownianMotion.java
@@ -1,26 +1,40 @@
 package arb.stochastic.processes;
 
+import arb.Real;
+
 /**
- * TODO
+ * dSₜ=μ*Sₜ*dt+ σ*Sₜ*dWₜ
  */
 public class GeometricBrownianMotion implements
                                      DiffusionProcess
 {
+  public final Real μ = new Real();
+  public final Real σ = new Real();
 
+  /**
+   * @return μ*Sₜ
+   */
   @Override
   public DriftCoeffecientFunction μ()
   {
-    assert false : "implement me";
-    return null;
-    
+    return (state, order, prec, result) ->
+    {
+      Real Sₜ = state.value;
+      return μ.mul(Sₜ, prec, result);
+    };
   }
 
+  /**
+   * @return σ*Sₜ
+   */
   @Override
   public DiffusionCoeffecientFunction σ()
   {
-    assert false : "implement me";
-    return null;
-    
+    return (state, order, prec, result) ->
+    {
+      Real Sₜ = state.value;
+      return σ.mul(Sₜ, prec, result);
+    };
   }
 
 }
