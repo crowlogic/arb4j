3a7c40d9c3e463a62c02685aa8890f497f0eed6f
Στεπηεν Cροωλεψ <c@3.po>
2022-08-03 17:43:43 -0500

    interesting stuff

---
+++ src/arb/functions/complex/elliptic/EllipticFunction.java
@@ -0,0 +1,14 @@
+package arb.functions.complex.elliptic;
+
+import arb.functions.complex.MeromorphicFunction;
+
+/**
+ * 
+ * @author crow
+ *
+ */
+public interface EllipticFunction extends
+                                  MeromorphicFunction
+{
+
+}
---
+++ src/arb/functions/complex/elliptic/FundamentalPeriodPair.java
@@ -0,0 +1,20 @@
+package arb.functions.complex.elliptic;
+
+import arb.Complex;
+import arb.OrderedPair;
+import arb.domains.ComplexPlane;
+
+/**
+ * A fundamental pair of periods is an {@link OrderedPair} of {@link Complex}
+ * numbers that define a lattice in the {@link ComplexPlane}. This type of
+ * lattice is the underlying object with which {@link EllipticFunction} and
+ * modular forms are defined.
+ * 
+ * @see <a href=
+ *      "https://en.wikipedia.org/wiki/Fundamental_pair_of_periods">Wikipedia</a>
+ */
+public class FundamentalPeriodPair extends
+                                   OrderedPair<Complex, Complex>
+{
+
+}
