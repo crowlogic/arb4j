fe8a358bbac7b961befcd7cbfc4c4cc100f485b3
Στεπηεν Cροωλεψ <c@3.po>
2022-08-04 01:07:35 -0500

    u will be assimilated

--- src/arb/geometry/surfaces/RiemannSurface.java
+++ src/arb/geometry/surfaces/RiemannSurface.java
@@ -1,17 +1,26 @@
 package arb.geometry.surfaces;
 
+import arb.Complex;
+import arb.Real;
+import arb.domains.ComplexPlane;
+import arb.functions.complex.HolomorphicFunction;
+import arb.geometry.Manifold;
+import arb.topological.spaces.MetrizableSpace;
+
 /**
- * A Riemann surface is a connected 1-dimensional complex manifold which can be
- * thought of as a deformed version of the complex plane: locally near every
- * point they look like patches of the complex plane, but the global topology
- * can be quite different. <br>
+ * A {@link RiemannSurface} is a connected 1-dimensional {@link Complex}
+ * {@link Manifold} which can be thought of as a deformed version of the
+ * {@link ComplexPlane}: locally near every point they look like patches of the
+ * {@link ComplexPlane}, but the global topology can be quite different. <br>
  * 
- * Every Riemann surface is a two-dimensional real analytic manifold (i.e., a
- * surface), but it contains more structure (specifically a complex structure)
- * which is needed for the unambiguous definition of holomorphic functions. <br>
+ * Every {@link RiemannSurface} is a two-dimensional Real analytic {@link Manifold}
+ * (i.e., a {@link Surface}), but it contains more structure (specifically a
+ * complex structure) which is needed for the unambiguous definition of
+ * {@link HolomorphicFunction}s. <br>
  * 
- * A two-dimensional real manifold can usually be turned into a Riemann surface
- * in several inequivalent ways if and only if it is orientable and metrizable.
+ * A two-dimensional {@link Real} {@link Manifold} can usually be turned into a
+ * {@link RiemannSurface} in several inequivalent ways if and only if it is
+ * orientable and a {@link MetrizableSpace}.
  * 
  */
 public interface RiemannSurface extends
