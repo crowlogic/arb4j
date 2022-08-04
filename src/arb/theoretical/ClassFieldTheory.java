2d372efe4ab7311de167a41c97dec0552356f825
Στεπηεν Cροωλεψ <c@3.po>
2022-08-03 22:35:05 -0500

    * @see <a href="doc-files/KMSStatesAndComplexMultiplication.pdf">KMS States and  *      Complex Multiplication</a> for relations between non-commutative  *      geometry and class field theory

--- src/arb/theoretical/ClassFieldTheory.java
+++ src/arb/theoretical/ClassFieldTheory.java
@@ -5,6 +5,9 @@ package arb.theoretical;
  * that describes abelian Galois extensions of local and global fields using
  * objects associated to the ground field
  * 
+ * @see <a href="doc-files/KMSStatesAndComplexMultiplication.pdf">KMS States and
+ *      Complex Multiplication</a> for relations between non-commutative
+ *      geometry and class field theory
  */
 public interface ClassFieldTheory extends
                                   Theory
