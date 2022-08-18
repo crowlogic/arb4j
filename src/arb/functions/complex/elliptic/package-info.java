e58699eb446d6e5e6e3b3b1bea64b3c685f1d6d0
Στεπηεν Cροωλεψ <cc@.>
2022-08-18 18:48:19 -0500

    describe package

---
+++ src/arb/functions/complex/elliptic/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * {@link arb.functions.complex.elliptic.EllipticFunction}s are a special kind
+ * of {@link arb.functions.complex.MeromorphicFunction}s, that satisfy two
+ * periodicity conditions. They are named
+ * {@link arb.functions.complex.elliptic.EllipticFunction}s because they come
+ * from elliptic integrals. Originally those integrals appeared in the
+ * calculation of an ellipses' arc length.
+ * 
+ * Important elliptic functions are
+ * {@link arb.functions.complex.numbertheoretic.JacobiθFunction}s and the
+ * Weierstrass ℘-function.
+ * 
+ * Further development of this theory led to hyperelliptic functions and modular
+ * forms.
+ * 
+ * @see <a href=
+ *      "https://en.wikipedia.org/wiki/Elliptic_function">EllipticFunction@Wikipedia</a>
+ */
+package arb.functions.complex.elliptic;
