61fb82f1764e63ef65af3bf2753ce15950631c7a
Στεπηεν Cροωλεψ <cc@.>
2022-08-25 18:50:59 -0500

    first pass at correlated random vector generator

--- src/arb/stochastic/processes/CorrelatedRandomVectorGenerator.java
+++ src/arb/stochastic/processes/CorrelatedRandomVectorGenerator.java
@@ -44,147 +44,149 @@ import arb.RealMatrix;
  * @since 1.2
  */
 
-//public class CorrelatedRandomVectorGenerator implements
-//                                             RandomVectorGenerator
-//{
-//  /** Mean vector. */
-//  private final Real                  mean;
-//
-//  /** Storage for the normalized vector. */
-//  private final Real                  normalized;
-//  /** Root of the covariance matrix. */
-//  private final RealMatrix                root;
-//  private RandomState randomState;
-//
-//  /**
-//   * Builds a correlated random vector generator from its mean vector and
-//   * covariance matrix.
-//   *
-//   * @param mean       Expected mean values for all components.
-//   * @param covariance Covariance matrix.
-//   * @param small      Diagonal elements threshold under which column are
-//   *                   considered to be dependent on previous ones and are
-//   *                   discarded
-//   * @param generator  underlying generator for uncorrelated normalized
-//   *                   components.
-//   * @
-//   */
-//  public CorrelatedRandomVectorGenerator(Real mean,
-//                                         RealMatrix covariance,
-//                                         RandomState randomState)
-//  {
-//    int order = covariance.getRows();
-//    assert mean.dim == order;
-//    this.mean = mean;
-//
-//
-//    this.randomState = randomState;
-//    normalized     = Real.newVector(order);
-//
-//  }
-//
-//  /**
-//   * Builds a null mean random correlated vector generator from its covariance
-//   * matrix.
-//   *
-//   * @param covariance Covariance matrix.
-//   * @param small      Diagonal elements threshold under which column are
-//   *                   considered to be dependent on previous ones and are
-//   *                   discarded.
-//   * @param generator  Underlying generator for uncorrelated normalized
-//   *                   components.
-//   * @throws org.apache.commons.math3.linear.NonPositiveDefiniteMatrixException if
-//   *                                                                            the
-//   *                                                                            covariance
-//   *                                                                            matrix
-//   *                                                                            is
-//   *                                                                            not
-//   *                                                                            strictly
-//   *                                                                            positive
-//   *                                                                            definite.
-//   */
-//  public CorrelatedRandomVectorGenerator(RealMatrix covariance, double small, NormalizedRandomGenerator generator)
-//  {
-//    int order = covariance.getRowDimension();
-//    mean = new double[order];
-//    for (int i = 0; i < order; ++i)
-//    {
-//      mean[i] = 0;
-//    }
-//
-//    final RectangularCholeskyDecomposition decomposition = new RectangularCholeskyDecomposition(covariance,
-//                                                                                                small);
-//    root           = decomposition.getRootMatrix();
-//
-//    this.generator = generator;
-//    normalized     = new double[decomposition.getRank()];
-//
-//  }
-//
-//  /**
-//   * Get the underlying normalized components generator.
-//   * 
-//   * @return underlying uncorrelated components generator
-//   */
-//  public NormalizedRandomGenerator getGenerator()
-//  {
-//    return generator;
-//  }
-//
-//  /**
-//   * Get the rank of the covariance matrix. The rank is the number of independent
-//   * rows in the covariance matrix, it is also the number of columns of the root
-//   * matrix.
-//   * 
-//   * @return rank of the square matrix.
-//   * @see #getRootMatrix()
-//   */
-//  public int getRank()
-//  {
-//    return normalized.length;
-//  }
-//
-//  /**
-//   * Get the root of the covariance matrix. The root is the rectangular matrix
-//   * <code>B</code> such that the covariance matrix is equal to
-//   * <code>B.B<sup>T</sup></code>
-//   * 
-//   * @return root of the square matrix
-//   * @see #getRank()
-//   */
-//  public RealMatrix getRootMatrix()
-//  {
-//    return root;
-//  }
-//
-//  /**
-//   * Generate a correlated random vector.
-//   * 
-//   * @return a random vector as an array of double. The returned array is created
-//   *         at each call, the caller can do what it wants with it.
-//   */
-//  public double[] nextVector()
-//  {
-//
-//    // generate uncorrelated vector
-//    for (int i = 0; i < normalized.length; ++i)
-//    {
-//      normalized[i] = generator.nextNormalizedDouble();
-//    }
-//
-//    // compute correlated vector
-//    double[] correlated = new double[mean.length];
-//    for (int i = 0; i < correlated.length; ++i)
-//    {
-//      correlated[i] = mean[i];
-//      for (int j = 0; j < root.getColumnDimension(); ++j)
-//      {
-//        correlated[i] += root.getEntry(i, j) * normalized[j];
-//      }
-//    }
-//
-//    return correlated;
-//
-//  }
-//
-//}
+public class CorrelatedRandomVectorGenerator implements
+                                             RandomVectorGenerator,
+                                             AutoCloseable
+{
+  /** Mean vector. */
+  private final Real       mean;
+
+  /** Storage for the normalized vector. */
+  private final Real       normalized;
+  /** Root of the covariance matrix. */
+  private final RealMatrix root;
+  private RandomState      randomState;
+
+  /**
+   * Builds a correlated random vector generator from its mean vector and
+   * covariance matrix.
+   *
+   * @param mean       Expected mean values for all components.
+   * @param covariance Covariance matrix.
+   * @param small      Diagonal elements threshold under which column are
+   *                   considered to be dependent on previous ones and are
+   *                   discarded
+   * @param generator  underlying generator for uncorrelated normalized
+   *                   components. @
+   */
+  public CorrelatedRandomVectorGenerator(Real mean, RealMatrix covariance, RandomState randomState)
+  {
+    int order = covariance.getRows();
+    assert mean.dim == order;
+    this.mean        = mean;
+    this.root        = RealMatrix.newMatrix(order, order);
+
+    this.randomState = randomState;
+    normalized       = Real.newVector(order);
+
+  }
+
+  /**
+   * Builds a null mean random correlated vector generator from its covariance
+   * matrix.
+   *
+   * @param covariance Covariance matrix.
+   * @param small      Diagonal elements threshold under which column are
+   *                   considered to be dependent on previous ones and are
+   *                   discarded.
+   * @param generator  Underlying generator for uncorrelated normalized
+   *                   components.
+   * @throws org.apache.commons.math3.linear.NonPositiveDefiniteMatrixException if
+   *                                                                            the
+   *                                                                            covariance
+   *                                                                            matrix
+   *                                                                            is
+   *                                                                            not
+   *                                                                            strictly
+   *                                                                            positive
+   *                                                                            definite.
+   */
+  public CorrelatedRandomVectorGenerator(RealMatrix covariance, int prec, RandomState randomState)
+  {
+    int order = covariance.getRows();
+    mean = new Real(order);
+    covariance.chol(prec, root = RealMatrix.newMatrix(order, order));
+    this.randomState = randomState;
+    normalized       = Real.newVector(order);
+
+  }
+
+  /**
+   * Get the underlying normalized components generator.
+   * 
+   * @return underlying uncorrelated components generator
+   */
+  public RandomState getRandomState()
+  {
+    return randomState;
+  }
+
+  /**
+   * Get the rank of the covariance matrix. The rank is the number of independent
+   * rows in the covariance matrix, it is also the number of columns of the root
+   * matrix.
+   * 
+   * @return rank of the square matrix.
+   * @see #getRootMatrix()
+   */
+  public int getRank()
+  {
+    return normalized.dim;
+  }
+
+  /**
+   * Get the root of the covariance matrix. The root is the rectangular matrix
+   * <code>B</code> such that the covariance matrix is equal to
+   * <code>B.B<sup>T</sup></code>
+   * 
+   * @return root of the square matrix
+   * @see #getRank()
+   */
+  public RealMatrix getRootMatrix()
+  {
+    return root;
+  }
+
+  /**
+   * 
+   * @param prec precision
+   * @param x    result
+   * @return result
+   */
+  public Real nextVector(int prec, Real x)
+  {
+
+    // generate uncorrelated vector
+    for (int i = 0; i < normalized.dim; ++i)
+    {
+      normalized.get(i).random(randomState, prec);
+    }
+
+    // compute correlated vector
+    Real correlated = Real.newVector(mean.dim);
+    try ( Real t = new Real();)
+    {
+      for (int i = 0; i < correlated.dim; ++i)
+      {
+        Real ci = correlated.get(i);
+        ci.set(mean.get(i));
+        for (int j = 0; j < root.getCols(); ++j)
+        {
+          ci.add(root.get(i, j).mul(normalized.get(j), prec, t), prec);
+        }
+      }
+
+    }
+    return correlated;
+
+  }
+
+  @Override
+  public void close() throws Exception
+  {
+    mean.close();
+    normalized.close();
+    root.close();
+  }
+
+}
