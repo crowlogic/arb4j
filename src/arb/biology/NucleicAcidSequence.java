0bdacce2eeff14a42c64876614ee3c1e7f6bcbcb
Στεπηεν Cροωλεψ <cc@.>
2022-09-01 21:14:36 -0500

    Scooby Doodby Do, where are you? We got some work to do now

---
+++ src/arb/biology/NucleicAcidSequence.java
@@ -0,0 +1,25 @@
+package arb.biology;
+
+/**
+ * A nucleic acid sequence is a succession of bases signified by a series of a
+ * set of five different letters that indicate the order of nucleotides forming
+ * alleles within a DNA (using GACT) or RNA (GACU) molecule. <br>
+ * By convention, sequences are usually presented from the 5' end to the 3' end.
+ * For DNA, the sense strand is used. Because nucleic acids are normally linear
+ * (unbranched) polymers, specifying the sequence is equivalent to defining the
+ * covalent structure of the entire molecule. For this reason, the nucleic acid
+ * sequence is also termed the primary structure.
+ * 
+ * The sequence has capacity to represent information. <br>
+ * Biological deoxyribonucleic acid represents the information which directs the
+ * functions of an organism. <br>
+ * 
+ * Nucleic acids also have a secondary structure and tertiary structure. Primary
+ * structure is sometimes mistakenly referred to as primary sequence.
+ * Conversely, there is no parallel concept of secondary or tertiary sequence.
+ *
+ */
+public interface NucleicAcidSequence
+{
+
+}
---
+++ src/arb/biology/proteins/SCUB3.java
@@ -0,0 +1,41 @@
+package arb.biology.proteins;
+
+import arb.biology.Gene;
+import arb.biology.Protein;
+
+/**
+ * This gene encodes a member of the signal peptide, complement subcomponents
+ * C1r/C1s, Uegf, bone morphogenetic protein-1 and epidermal growth factor-like
+ * domain containing protein family. <br>
+ * 
+ * Overexpression of this gene in human embryonic kidney cells results in
+ * secretion of a glycosylated form of the protein that forms oligomers and
+ * tethers to the cell surface. <br>
+ * 
+ * This gene is upregulated in lung cancer tumor tissue compared to healthy
+ * tissue and is associated with loss of the epithelial marker E-cadherin and
+ * with increased expression of vimentin, a mesenchymal marker. <br>
+ * 
+ * In addition, the protein encoded by this gene is a transforming growth factor
+ * beta receptor ligand, and when secreted by cancer cells, it can be cleaved in
+ * vitro to release the N-terminal epidermal growth factor-like repeat domain
+ * and the C-terminal complement subcomponents C1r/C1s domain.<br>
+ * 
+ * Both the full length protein and C-terminal fragment can bind to the
+ * transforming growth factor beta type II receptor to promote the
+ * epithelial-mesenchymal transition and tumor angiogenesis. <br>
+ * 
+ * Alternative splicing results in multiple transcript variants. [provided by
+ * RefSeq, Dec 2014]
+ * 
+ * @see <a href=
+ *      "https://www.proteinatlas.org/ENSG00000146197-SCUBE3">SCUBE3@TheHumanProteinAtlas</a>
+ *      <a href=
+ *      "https://www.sciencedaily.com/releases/2022/06/220630142211.htm">Signaling
+ *      molecule potently stimulates hair growth@ScienceDaily</a>
+ */
+public interface SCUB3 extends
+                       Gene
+{
+
+}
