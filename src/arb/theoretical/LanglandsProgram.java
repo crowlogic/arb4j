eb228897fa190c2128efd2311cb65c39d307e8f8
Stephen Crowley <crowlogic@proton.me>
2022-10-02 16:29:52 -0500

    more concise and relevant summary for the Langlands program

--- src/arb/theoretical/LanglandsProgram.java
+++ src/arb/theoretical/LanglandsProgram.java
@@ -3,24 +3,11 @@ package arb.theoretical;
 import arb.*;
 
 /**
- * The Langlands program is a grand unified theory of mathematics which includes
- * the search for {@link LanglandsReciprocity} which is a generalization of
- * {@link ArtinReciprocity} to non-Abelian Galois extensions of
- * {@link Field}s<br>
- * 
- * Langlands proposed that the mathematics of algebra of Galois representations
- * and analysis of automorphic forms are intimately related, and that
- * congruences over finite fields are related to infinite-dimensional
- * representation theory.<br.
- * 
- * In particular, Langlands particularly conjectured that the transformations
- * behind general {@link Reciprocity} laws could be represented by means of
- * matrices (such as {@link RealMatrix} and {@link ComplexMatrix} )<br>
- * 
- * In 1998, three mathematicians proved Langlands' conjectures for local fields,
- * and in a November 1999 lecture at the Institute for Advanced Study at
- * Princeton University, L. Lafforgue presented a proof of the conjectures for
- * function fields. This leaves only the case of {@link Field}s unresolved.<br>
+ * As an analog to the possible exact distribution of primes, the Langlands
+ * program allows a potential general tool for the resolution of invariance at
+ * the level of generalized algebraic structures. This in turn permits a
+ * somewhat unified analysis of arithmetic objects through their automorphic
+ * functions
  * 
  * https://mathworld.wolfram.com/LanglandsProgram.html
  * https://en.wikipedia.org/wiki/Langlands_program
