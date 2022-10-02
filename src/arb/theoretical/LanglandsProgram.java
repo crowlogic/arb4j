55b786f939529e3c60c00d2dfcbf000c392c79a7
Stephen Crowley <crowlogic@proton.me>
2022-10-02 16:32:48 -0500

    potentiality

--- src/arb/theoretical/LanglandsProgram.java
+++ src/arb/theoretical/LanglandsProgram.java
@@ -7,14 +7,13 @@ import arb.*;
  * program allows a potential general tool for the resolution of invariance at
  * the level of generalized algebraic structures. This in turn permits a
  * somewhat unified analysis of arithmetic objects through their automorphic
- * functions
- * 
+ * functions <br>
  * <a href=
- * "https://mathworld.wolfram.com/LanglandsProgram.html">LanglandsProgram@Mathworld</a>
+ * "https://mathworld.wolfram.com/LanglandsProgram.html">LanglandsProgram@Mathworld</a><br>
  * <a href=
- * "https://en.wikipedia.org/wiki/Langlands_program">LanglandsProgram@Wikipedia</a>
+ * "https://en.wikipedia.org/wiki/Langlands_program">LanglandsProgram@Wikipedia</a><br>
  * <a href=
- * "https://www.ias.edu/ideas/modern-mathematics-and-langlands-program">ModernMathematicsAndTheLanglandsProgram@TheInstituteForAdvancedStudy</a>
+ * "https://www.ias.edu/ideas/modern-mathematics-and-langlands-program">ModernMathematicsAndTheLanglandsProgram@TheInstituteForAdvancedStudy</a><br>
  * 
  * @see {@link ArtinReciprocity} as well as Automorphic Forms
  * @see {@link LanglandsReciprocity}
