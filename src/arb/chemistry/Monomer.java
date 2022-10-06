b914aafdd6e086d49984e8403a984a2a56949680
Stephen Crowley <crowlogic@proton.me>
2022-10-05 19:38:05 -0500

    modelling

---
+++ src/arb/chemistry/Monomer.java
@@ -0,0 +1,26 @@
+package arb.chemistry;
+
+/**
+ * <pre>
+  From The Collaborative International Dictionary of English v.0.48 [gcide]:
+
+  monomer \mon"o*mer\, n. (Chem.)
+     The basic conceptual building unit of a polymer; a molecule
+     of low molecular weight which may combine with other
+     molecules to form a molecule in a chain or branched form
+     having high molecular weight; as, amino acids are the monomer
+     units which are combined to form proteins; vinylic plastics
+     are formed from monomers having a vinyl group.
+     [PJC]
+
+From WordNet (r) 3.0 (2006) [wn]:
+
+  monomer
+      n 1: a simple compound whose molecules can join together to form
+           polymers
+ * </pre>
+ */
+public interface Monomer
+{
+
+}
