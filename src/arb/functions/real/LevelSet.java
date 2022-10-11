42df1fb88f67e160c0e23e7b41ba4504471fc0f8
Stephen Crowley <crowlogic@proton.me>
2022-10-11 01:00:59 -0500

    model level sets, curves, surfaces, and hypersurface

--- src/arb/functions/real/LevelSet.java
+++ src/arb/functions/real/LevelSet.java
@@ -1,28 +1,38 @@
 package arb.functions.real;
 
+import arb.Real;
+import arb.Set;
+import arb.functions.Function;
+import arb.geometry.curves.LevelCurve;
+import arb.geometry.surfaces.LevelHyperSurface;
+import arb.geometry.surfaces.LevelSurface;
 import arb.topology.Fiber;
 
 /**
- * a level set of a real-valued function f of n real variables is a set where
- * the function takes on a given constant value c, that is:
+ * A {@link LevelSet} of a {@link Real}-valued {@link Function} of n
+ * {@link Real} variables is a {@link Set} where the function takes on a given
+ * constant value <b>c</b>, that is:
  * 
- * L_c(f)={(x_1, … , x_n ) ∣ f ( x_1 , … , x_n ) = c } <br>
+ * L_c(f)={(x[1], … , x[n] ) ∣ f ( x[1] , … , x[n] ) = c } <br>
  * 
- * When the number of independent variables is two, a level set is called a
- * level curve, also known as contour line or isoline; so a level curve is the
- * set of all real-valued solutions of an equation in two variables x1 and x2.
- * When n = 3, a level set is called a level surface (or isosurface); so a level
- * surface is the set of all real-valued roots of an equation in three variables
- * x1, x2 and x3. For higher values of n, the level set is a level hypersurface,
- * the set of all real-valued roots of an equation in n > 3 variables.
+ * When the number of independent variables is two, a {@link LevelSet} is called
+ * a {@link LevelCurve}, also known as <i>contour line</i> or <i>isoline</i>; so
+ * a {@link LevelCurve} is the set of all {@link Real}-valued solutions of an
+ * equation in two variables x[1] and x[2]. <br>
  * 
- * A level set is a special case of a fiber.
  * 
- * @author crow
- *
+ * When n = 3, a {@link LevelSet} is called a {@link LevelSurface} (or
+ * isosurface); so a LevelSurface is the set of all {@link Real}-valued roots of
+ * an equation in three variables, x[1], x[2], and x[3]. For higher values of n,
+ * the level set is a {@link LevelHyperSurface} of all {@link Real}-valued roots
+ * of an equation in n > 3 variables.
+ * 
+ * A {@link LevelSet} is a special case of a {@link Fiber}
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Level_set">LevelSet@Wikipedia</a>
  */
-public interface LevelSet extends
-                          Fiber
+public interface LevelSet<F extends RealFunction> extends
+                         Fiber
 {
 
 }
