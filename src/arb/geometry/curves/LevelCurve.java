c2426f7d579044f4a5a7a60656750fa578442ebd
Stephen Crowley <crowlogic@proton.me>
2022-10-11 01:21:21 -0500

    2do

--- src/arb/geometry/curves/LevelCurve.java
+++ src/arb/geometry/curves/LevelCurve.java
@@ -1,7 +1,12 @@
 package arb.geometry.curves;
 
 import arb.functions.real.LevelSet;
+import arb.functions.real.dynamics.RealNewtonMapTest;
 
+/**
+ * TODO: implement this with the guts of {@link RealNewtonMapTest}
+ * 
+ */
 public interface LevelCurve extends
                             LevelSet
 {
