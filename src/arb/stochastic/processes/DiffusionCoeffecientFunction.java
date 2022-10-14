084723c4949aa488db6d26b37814e71cf6c369c1
Στεπηεν Cροωλεψ <cc@.>
2022-10-13 19:25:26 -0500

    put that back

--- src/arb/stochastic/processes/DiffusionCoeffecientFunction.java
+++ src/arb/stochastic/processes/DiffusionCoeffecientFunction.java
@@ -5,5 +5,8 @@ import arb.dynamical.systems.*;
 public interface DiffusionCoeffecientFunction<S extends State> extends
                                              CoeffecientFunction<S>
 {
- 
+  default boolean dependsOnAbsoluteTime()
+  {
+    return false;
+  }
 }
