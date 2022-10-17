582a136e189ea3293d1ce07347fcafcda94db3c6
Stephen Crowley <crowlogic@proton.me>
2022-10-17 04:21:41 -0500

    coherence++

--- src/arb/groups/CoveringGroup.java
+++ src/arb/groups/CoveringGroup.java
@@ -1,9 +1,11 @@
 package arb.groups;
 
+import arb.algebraic.topology.Homomorphism;
+
 /**
  * a covering group of a topological group H is a covering space G of H such
  * that G is a topological group and the covering map p : G → H is a continuous
- * group homomorphism. The map p is called the covering homomorphism. A
+ * Group {@link Homomorphism}. The map p is called the covering {@link Homomorphism}. A
  * frequently occurring case is a double covering group, a topological double
  * cover in which H has index 2 in G; examples include the spin groups, pin
  * groups, and metaplectic groups.
--- src/arb/theoretical/MonstrousMoonshineTheory.java
+++ src/arb/theoretical/MonstrousMoonshineTheory.java
@@ -1,5 +1,13 @@
 package arb.theoretical;
 
+import arb.geometry.algebraic.ModularCurve;
+
+/**
+ * {@link ModularCurve}s of genus 0 are rare compared to those of higher genus. They are important because the normalizer 
+ * Γ_0(p)^* of Γ_0(p) in SL(2,R) (the   
+ * @author crow
+ *
+ */
 public interface MonstrousMoonshineTheory extends
                                           Theory
 {
