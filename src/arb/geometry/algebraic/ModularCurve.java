f7abd3e69cec1d0bef27f866e4f609c90de6c0db
Στεπηεν Cροωλεψ <cc@.>
2022-10-22 22:54:50 -0500

    modular todo

--- src/arb/geometry/algebraic/ModularCurve.java
+++ src/arb/geometry/algebraic/ModularCurve.java
@@ -1,11 +1,13 @@
 package arb.geometry.algebraic;
 
 import arb.geometry.surfaces.RiemannSurface;
+import arb.groups.ModularGroup;
 
 /**
  * modular curves are {@link ModuliSpace}s for {@link EllipticCurve}s with
  * {@link LevelStructure}
  * 
+ * TODO: relate to the {@link ModularGroup}
  */
 public interface ModularCurve<E extends EllipticCurve, L extends LevelStructure<E>> extends
                              ModuliSpace<L>,
---
+++ src/arb/groups/ModularGroup.java
@@ -0,0 +1,7 @@
+package arb.groups;
+
+public interface ModularGroup extends
+                              Group
+{
+
+}
