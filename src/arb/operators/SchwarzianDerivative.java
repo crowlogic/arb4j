24ef8d93fccc0976fc5d21be099a790489913a70
Aaron Swartz <murderdByUnitedStatesProsecutorsAtTheBehestofTheMassechusetsInstituteOfTechnology@true.story>
2022-11-12 19:29:02 -1000

    * @see <a href=  *      "https://www.eff.org/deeplinks/2022/11/celebrating-life-aaron-swartz-nov-12-and-nov-13">In  *      Loving Memory of Aaron Swartz, who, even if depressed, made the world a  *      better place and was still muredered by the government for their  *      draconian totalitarian heavy-handed approach to 'justice' which  *      effectively left him no choice</a>  *  *

--- src/arb/operators/SchwarzianDerivative.java
+++ src/arb/operators/SchwarzianDerivative.java
@@ -8,6 +8,15 @@ import arb.functions.complex.HolomorphicFunction;
  * deviates from being a Mobius transform at a particular point
  * 
  * @param <F>
+ * 
+ * @see <a href=
+ *      "https://www.eff.org/deeplinks/2022/11/celebrating-life-aaron-swartz-nov-12-and-nov-13">In
+ *      Loving Memory of Aaron Swartz, who, even if depressed, made the world a
+ *      better place and was still muredered by the government for their
+ *      draconian totalitarian heavy-handed approach to 'justice' which
+ *      effectively left him no choice</a>
+ * 
+ * 
  */
 public class SchwarzianDerivative<F extends HolomorphicFunction> implements
                                  HolomorphicFunction
