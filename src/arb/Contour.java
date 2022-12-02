d615d72d6030be1cfaf7510c7b486b471a94fe8d
Stephen Crowley <crowlogic@proton.me>
2022-12-02 17:13:47 -0600

    fuck univalent functions cause they are fucking useless academic shit

--- src/arb/Contour.java
+++
@@ -1,9 +0,0 @@
-package arb;
-
-import arb.differential.geometry.homotopy.Path;
-
-public interface Contour extends
-                         Path
-{
-
-}
--- src/arb/functions/complex/univalent/package-info.java
+++
@@ -1,1 +0,0 @@
-package arb.functions.complex.univalent;
