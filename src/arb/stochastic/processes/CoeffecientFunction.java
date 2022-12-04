19c0cdb1f44b8864629afc152f94cf8355e02fd7
Stephen Crowley <crowlogic@proton.me>
2022-12-03 18:55:39 -0600

    ...

--- src/arb/stochastic/processes/CoeffecientFunction.java
+++ src/arb/stochastic/processes/CoeffecientFunction.java
@@ -7,6 +7,11 @@ import arb.functions.Function;
 public interface CoeffecientFunction<S extends State> extends
                                     Function<S, Real>
 {
+  /**
+   * 
+   * @return true if this function explicitly depends on time rather than just on
+   *         the state variables which are functions of time
+   */
   default boolean dependsOnAbsoluteTime()
   {
     return false;
