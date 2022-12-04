8af31acc6851c98216e42c3140c15324b0570f47
StephenCrowley <stephencrowley214@gmail.com>
2022-12-04 05:18:28 -0600

    coooooool

--- src/arb/viz/CurveRenderer.java
+++ src/arb/viz/CurveRenderer.java
@@ -18,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -28,15 +29,38 @@ public class CurveRenderer extends
 {
 
   private static final int prec = 128;
-  private static final int W     = 1280;
-  private static final int H     = 720;
+  private static final int W    = 1280;
+  private static final int H    = 720;
 
-  private GraphicsContext  g;
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
-  private double           t     = 0.0;
-  private double           oldX  = W / 2, oldY = H / 2;
+  private GraphicsContext g;
+  private double          t          = 0.0;
 
-  private boolean          start = true;
+  private double          oldX       = W / 2, oldY = H / 2;
+
+  private boolean         start      = true;
+
+  Lemniscate              lemniscate = new Lemniscate();
+
+  Real                    realt      = new Real();
+
+  Complex                 z          = new Complex();
+
+  @Override
+  public void close() throws Exception
+  {
+    realt.close();
+    z.close();
+  }
+
+  private static int randomEightBitInteger()
+  {
+    return (int) (Math.random() * 255);
+  }
 
   private Parent createContent()
   {
@@ -47,7 +71,8 @@ public class CurveRenderer extends
                                H);
     g = canvas.getGraphicsContext2D();
     g.setLineWidth(3);
-    
+    root.setBackground(Background.fill(Color.BLACK));
+
     AnimationTimer timer = new AnimationTimer()
     {
       @Override
@@ -56,12 +81,14 @@ public class CurveRenderer extends
         if (!start)
           return;
 
-        for (int i = 0; i < 80; i++)
+        for (int i = 0; i < 5; i++)
         {
           t += 0.01;
           draw();
         }
+        g.setStroke(Color.rgb(randomEightBitInteger(), randomEightBitInteger(), randomEightBitInteger()));
       }
+
     };
     timer.start();
 
@@ -71,17 +98,17 @@ public class CurveRenderer extends
 
   private void draw()
   {
-    Point2D p = curveFunction();
+    Point2D p    = evaluateCurvePosition();
 
-    g.setStroke(Color.BLACK);
+    // g.setStroke(Color.BLACK);
 
-    double newX = W / 2 + p.getX();
-    double newY = H / 2 + p.getY();
+    double  newX = W / 2 + p.getX();
+    double  newY = H / 2 + p.getY();
 
     if (oldX != W / 2 && oldY != H / 2)
     {
       g.strokeLine(oldX, oldY, newX, newY);
-      // g.strokeOval(newX, newY, 1, 1);
+
     }
     //
 
@@ -89,28 +116,12 @@ public class CurveRenderer extends
     oldY = newY;
   }
 
-  Lemniscate lemniscate = new Lemniscate();
-
-  Real       realt      = new Real();
-
-  Complex    z          = new Complex();
-
-  private Point2D curveFunction()
+  private Point2D evaluateCurvePosition()
   {
     lemniscate.evaluate(realt.set(t), 1, prec, z).mul(200, prec);
 
     return new Point2D(z.getReal().doubleValue(),
                        z.getImag().doubleValue());
-  }
-
-  private void saveScreenshot(Scene scene) throws IOException
-  {
-    WritableImage fxImage  = scene.snapshot(null);
-
-    BufferedImage awtImage = SwingFXUtils.fromFXImage(fxImage, null);
-
-    ImageIO.write(awtImage, "png", new File("screenshot.png"));
-
   }
 
   @Override
@@ -120,17 +131,5 @@ public class CurveRenderer extends
 
     stage.setScene(scene);
     stage.show();
-  }
-
-  public static void main(String[] args)
-  {
-    launch(args);
-  }
-
-  @Override
-  public void close() throws Exception
-  {
-    realt.close();
-    z.close();
   }
 }
