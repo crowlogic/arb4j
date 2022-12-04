b633a3425e2186b6d44c811703dc48f21c453c9b
Stephen Crowley <crowlogic@proton.me>
2022-12-04 00:58:19 -0600

    ...

--- src/arb/calculus/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Calculus, originally called infinitesimal calculus or "the calculus of
- * infinitesimals", is the mathematical study of continuous change, in the same
- * way that geometry is the study of shape, and algebra is the study of
- * generalizations of arithmetic operations.
- */
-package arb.calculus;
