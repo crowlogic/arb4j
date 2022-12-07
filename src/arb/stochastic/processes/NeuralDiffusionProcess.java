8dfb1f40b06c3ef8f7c7c0854606a45479145821
Stephen Crowley <crowlogic@proton.me>
2022-12-07 15:03:36 -0600

    Revert "fuck that"
    This reverts commit 307516bf29dd864ce580b93d2a3744e99aa92876.

---
+++ src/arb/stochastic/processes/NeuralDiffusionProcess.java
@@ -0,0 +1,13 @@
+package arb.stochastic.processes;
+
+/**
+ * TODO: implement
+ * 
+ * @see <a href="doc-files/NeuralDiffusionProcess.pdf">Neural Diffusion
+ *      Processes</a>
+ */
+public abstract class NeuralDiffusionProcess implements
+                                             DiffusionProcess
+{
+
+}
