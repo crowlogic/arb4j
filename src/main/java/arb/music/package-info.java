652ab60fda1f08453382069c463260c2526eb4a1
Stephen A. Crowley <stephencrowley214@gmail.com>
2023-08-21 01:24:00 -0500

    import(ant) note

--- src/main/java/arb/music/package-info.java
+++ src/main/java/arb/music/package-info.java
@@ -1,5 +1,10 @@
 /**
  * The classes in this package are intended to be structured to provide a clear
- * and type-safe hierarchy for representing musical constructs
+ * and type-safe hierarchy for representing musical constructs and eventually
+ * make something as astoundingly magnificent as
+ * <a href="https://www.youtube.com/watch?v=FX8hBmgAbsM">Sebastian Sellares -
+ * Garden of Eden</a> which I was fortunate enough to stumble upon when i
+ * randomly decided to see what typing Eden into yewtube would greet me with and
+ * was very pleased that I did
  */
 package arb.music;
