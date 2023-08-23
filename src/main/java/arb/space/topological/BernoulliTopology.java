af8af3e7eb81fd43815464fac683b0bc89653ba0
Stephen Crowley <stephencrowley214@gmail.com>
2023-08-22 22:25:45 -0500

    convolve

---
+++ src/main/java/arb/space/topological/BernoulliTopology.java
@@ -0,0 +1,14 @@
+package arb.space.topological;
+
+import arb.Field;
+
+/**
+ * TODO: implement
+ * 
+ * @param <X>
+ */
+public interface BernoulliTopology<X extends Field<X>> extends
+                                  Topology<X>
+{
+
+}
