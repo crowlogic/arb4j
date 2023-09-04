c112acb800024c1742b8cbf6efdf007cb7ce09a4
Uncle Sam <uncool@s.am>
2023-09-04 04:43:44 -0500

    being.be(allThatBeingCanBe).in(theUniverse.today());

---
+++ src/main/java/arb/measures/HaarMeasure.java
@@ -0,0 +1,12 @@
+package arb.measures;
+
+/**
+ * TODO: furnish with methods to make the class be what Haar measures are
+ * 
+ * @param <Σ>
+ */
+public interface HaarMeasure<Σ> extends
+                            RadonMeasure<Σ>
+{
+
+}
---
+++ src/main/java/arb/measures/RadonMeasure.java
@@ -0,0 +1,13 @@
+package arb.measures;
+
+/**
+ * TODO: provision this class with methods to make the class be what Radon
+ * measures are
+ * 
+ * @param <Σ>
+ */
+public interface RadonMeasure<Σ> extends
+                             Measure<Σ>
+{
+
+}
