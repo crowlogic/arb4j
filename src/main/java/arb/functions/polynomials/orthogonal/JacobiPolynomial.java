9e9530a445717a9bed5a61ebaa89407614468232
Stephen A. Crowley <stephencrowley214@gmail.com>
2023-11-04 03:15:37 -0500

    * <pre>  * Copyright ©2023 Stephen Crowley  *  * This Source Code Form is subject to the terms of the Mozilla Public License,  * v. 2.0. If a copy of the MPL was not distributed with this file, You can  * obtain one at https://mozilla.org/MPL/2.0/.  * </pre>

--- src/main/java/arb/functions/polynomials/orthogonal/JacobiPolynomial.java
+++ src/main/java/arb/functions/polynomials/orthogonal/JacobiPolynomial.java
@@ -12,15 +12,30 @@ import arb.RealPolynomial;
  * For the Jacobi polynomials:
  * 
  * Initial condition: P₀(x) = 1 
+ * 
  * Initial condition: P₁(x) = ½*(α + β + 2) + x*(α - β)
  * 
  * Recurrence relation: Pₙ(x) = aₙ*x*Pₙ₋₁(x) + bₙ*Pₙ₋₂(x)
  * 
- * Common factor: cₙ = (2ₙ + α + β - 1) / ((2ₙ + α + β)*(2ₙ - 2 + α + β))
- * 
  * coefficients: aₙ = cₙ (α² - β²)
  *  
  * coeffecients: bₙ = cₙ (2ₙ + α + β)
+ * 
+ * where
+ * 
+ * Common factor: cₙ = (dₙ - 1) / (dₙ*dₙ₋₁)
+ * 
+ * with
+ * 
+ * Common factor: dₙ = 2n + α + β
+ * 
+ * <pre>
+ * Copyright ©2023 Stephen Crowley
+ *  
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at https://mozilla.org/MPL/2.0/.
+ * </pre>
  */
 public class JacobiPolynomial extends
                               RealPolynomial
