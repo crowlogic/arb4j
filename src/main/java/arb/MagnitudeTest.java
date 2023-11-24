885cfbe7bbaca1f73b2312634160113d5fbb8c88
Stephen A. Crowley <crow@outsider.trading>
2023-11-23 19:56:57 -0600

    * TODO: find out whats causing https://github.com/crowlogic/arb4j/issues/270

---
+++ src/main/java/arb/MagnitudeTest.java
@@ -0,0 +1,12 @@
+package arb;
+
+import junit.framework.TestCase;
+
+/**
+ * TODO: find out whats causing https://github.com/crowlogic/arb4j/issues/270
+ */
+public class MagnitudeTest extends
+                           TestCase
+{
+
+}
