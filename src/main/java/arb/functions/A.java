f364e9e3c4ec5f0bc03eaedb5c7c3289beedc33b
Stephen A. Crowley <crow@outsider.trading>
2024-01-07 18:48:45 -0600

    TODO: contextual functioncalls

---
+++ src/main/java/arb/functions/A.java
@@ -0,0 +1,29 @@
+package arb.functions;
+
+import arb.Integer;
+import arb.Real;
+
+public class A
+{
+  public Integer                 c0 = new Integer("1");
+  public Real                    l0 = new Real();
+  public Real                    l1 = new Real();
+  public Function<Integer, Real> C  = null;
+
+  public Real evaluate(Integer in, int order, int bits, Real result)
+  {
+    return ((Real) this.C.evaluate(((Integer) in).sub(this.c0, bits, (Real) result),
+                                   order,
+                                   bits,
+                                   this.l0)).mul((Real) this.C.evaluate((Integer) in, order, bits, this.l1),
+                                                 bits,
+                                                 (Real) result);
+  }
+
+  public void close()
+  {
+    this.c0.close();
+    this.l0.close();
+    this.l1.close();
+  }
+}
