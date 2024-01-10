6e974b98103618cc00fc30640d8a576fc47c7c2c
Stephen A. Crowley <crow@outsider.trading>
2024-01-10 16:37:03 -0600

    removed tests that were slowing things down,monte carlo sucks anyway

--- src/test/java/arb/RealTest.java
+++ src/test/java/arb/RealTest.java
@@ -204,19 +204,6 @@ public class RealTest extends
 
   }
 
-  public static void testNormalize()
-  {
-    Real r = Real.newVector(100000);
-    r.randomlyGenerate(new GaussianDistribution(RealConstants.zero,
-                                                new Real("2",
-                                                         128)),
-                       new RandomState(2022),
-                       prec);
-    assertEquals(2, r.standardDeviation(128, new Real()).doubleValue(), 0.01);
-    r.normalize(prec, r);
-    assertEquals(1, r.standardDeviation(128, new Real()).doubleValue(), Math.pow(10, -15));
-  }
-
   public static void testVecScalarSub()
   {
     Real r = Real.newVector(3);
