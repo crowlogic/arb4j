e6b3d8f4d20238105b027df374d26c2347a413f1
Stephen A. Crowley <crow@outsider.trading>
2024-01-14 17:46:42 -0600

    ...

--- src/main/java/arb/functions/package-info.java
+++ src/main/java/arb/functions/package-info.java
@@ -1,6 +1,1 @@
-/**
- * Using ball arithmetic, it is possible to do rigorous root-finding and
- * integration (among other operations) with generic functions. This code should
- * be considered experimental; use at your own risk.
- */
 package arb.functions;
