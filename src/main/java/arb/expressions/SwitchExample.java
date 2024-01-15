f5edd5810c9ec85d6e8032b917218e39d649a2f1
Stephen A. Crowley <crow@outsider.trading>
2024-01-15 02:39:03 -0600

    switch example

---
+++ src/main/java/arb/expressions/SwitchExample.java
@@ -0,0 +1,74 @@
+package arb.expressions;
+
+import static org.objectweb.asm.Opcodes.*;
+
+import org.objectweb.asm.*;
+
+public class SwitchExample
+{
+  public static void main(String[] args) throws Exception
+  {
+    ClassWriter   cw      = new ClassWriter(0);
+    // Omitted: class definition code
+
+    // Method for TableSwitchInsnNode example
+    MethodVisitor mvTable = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "testTableSwitch", "(I)V", null, null);
+    createTableSwitch(mvTable);
+
+    // Method for LookupSwitchInsnNode example
+    MethodVisitor mvLookup = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "testLookupSwitch", "(I)V", null, null);
+    createLookupSwitch(mvLookup);
+
+    // Omitted: class finalization and saving
+  }
+
+  private static void createTableSwitch(MethodVisitor mv)
+  {
+    mv.visitCode();
+    Label   defaultLabel = new Label();
+    Label[] labels       = new Label[]
+    { new Label(), new Label(), new Label() };
+
+    mv.visitVarInsn(ILOAD, 0);
+    mv.visitTableSwitchInsn(1, 3, defaultLabel, labels);
+
+    for (int i = 0; i < labels.length; i++)
+    {
+      mv.visitLabel(labels[i]);
+      // Your case code goes here
+      mv.visitJumpInsn(GOTO, defaultLabel);
+    }
+
+    mv.visitLabel(defaultLabel);
+    // Your default case code goes here
+    mv.visitInsn(RETURN);
+    mv.visitMaxs(3, 1);
+    mv.visitEnd();
+  }
+
+  private static void createLookupSwitch(MethodVisitor mv)
+  {
+    mv.visitCode();
+    Label   defaultLabel = new Label();
+    int[]   keys         =
+    { 10, 20, 30 };
+    Label[] labels       = new Label[]
+    { new Label(), new Label(), new Label() };
+
+    mv.visitVarInsn(ILOAD, 0);
+    mv.visitLookupSwitchInsn(defaultLabel, keys, labels);
+
+    for (int i = 0; i < labels.length; i++)
+    {
+      mv.visitLabel(labels[i]);
+      // Your case code goes here
+      mv.visitJumpInsn(GOTO, defaultLabel);
+    }
+
+    mv.visitLabel(defaultLabel);
+    // Your default case code goes here
+    mv.visitInsn(RETURN);
+    mv.visitMaxs(3, 1);
+    mv.visitEnd();
+  }
+}
