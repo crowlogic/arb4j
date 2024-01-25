53b74e76bee7d664018406d18b184dddccabbf5d
Stephen A. Crowley <crow@outsider.trading>
2024-01-24 19:53:38 -0600

    4

---
+++ src/main/java/arb/functions/polynomials/orthogonal/FourierTransformedType1ChebyshevPolynomialSequence.java
@@ -0,0 +1,34 @@
+package arb.functions.polynomials.orthogonal;
+
+import java.util.Iterator;
+
+import arb.domains.Domain;
+import arb.functions.complex.ComplexFunction;
+import arb.functions.real.RealFunction;
+
+public class FourierTransformedType1ChebyshevPolynomialSequence implements
+                                                                OrthogonalPolynomialSequence
+{
+
+  @Override
+  public Iterator<RealFunction> iterator()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public ComplexFunction getOrthogonalMeasure()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Domain<?> getDomain()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
