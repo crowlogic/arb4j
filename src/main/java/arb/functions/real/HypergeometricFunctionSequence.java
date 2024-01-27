ccc2e82aa05bebff46acd980f7aa3aff9917e4fb
Stephen A. Crowley <crow@outsider.trading>
2024-01-26 23:28:47 -0600

    F(0,x)=1 F(1,x)=x F(2,x)=x² F(3,x)=x³ F(4,x)=x⁴

--- src/main/java/arb/functions/real/HypergeometricFunctionSequence.java
+++ src/main/java/arb/functions/real/HypergeometricFunctionSequence.java
@@ -15,103 +15,36 @@ public class HypergeometricFunctionSequence implements
                                             Function<Integer, RealPolynomial>
 {
 
-  public static class F implements
-                        Function<Integer, RealPolynomial>
-  {
-    private boolean       isInitialized;
-    Integer               c1  = new Integer("0");
-    Integer               c2  = new Integer("1");
-    public Real           α;
-    public Real           β;
-    public Integer        ℤ1  = new Integer();
-    public RealPolynomial r̅1 = new RealPolynomial();
-    public F              F;
-
-    public RealPolynomial evaluate(Integer in, int order, int bits, RealPolynomial result)
-    {
-      if (!isInitialized)
-      {
-        initialize();
-      }
-      return switch (in.getSignedValue())
-      {
-      case 0 -> result.set(c2);
-      default ->
-      {
-        RealPolynomial var5 = result.identity();
-        if (F == null)
-        {
-          F = new F(this);
-        }
-
-        F.evaluate(in.sub(c2, bits, ℤ1), order, bits, r̅1);
-        yield var5.mul(r̅1, bits, result);
-      }
-      };
-    }
-
-    public F()
-    {
-    }
-
-    public void initialize()
-    {
-      if (isInitialized)
-      {
-        throw new AssertionError("Already initialized");
-      }
-      else
-      {
-        isInitialized = true;
-      }
-    }
-
-    public F(F var1)
-    {
-      this();
-
-    }
-
-    public void close()
-    {
-      c1.close();
-      c2.close();
-      ℤ1.close();
-      r̅1.close();
-      F.close();
-    }
-  }
-
   public static void main(String... args)
   {
-//    HypergeometricFunctionSequence F  = new HypergeometricFunctionSequence(3,
-//                                                                           1);
-    var            f  = new F();
-//    f.α.set(1, 4, -4);
-//    f.β.set(0.5);
+    try ( var F = new HypergeometricFunctionSequence(3,
+                                                     1);
+          var index = new Integer())
+    {
+      F.α.set(1, 4, -4);
+      F.β.set(0.5);
 
-    RealPolynomial F0 = f.evaluate(new Integer(0), 0, 128, new RealPolynomial());
-    out.println("F0=" + F0);
-    RealPolynomial F1 = f.evaluate(new Integer(1), 0, 128, new RealPolynomial());
-    out.println("F1=" + F1);
-    RealPolynomial F2 = f.evaluate(new Integer(2), 0, 128, new RealPolynomial());
-    out.println("F2=" + F2);
+      for (int n = 0; n < 5; n++)
+      {
+        RealPolynomial fn = F.evaluate(index.set(n), 0, 128, new RealPolynomial());
+        out.format("F(%d,x)=%s\n", n, fn);
+      }
+    }
   }
 
-  public final Context                     context;
+  public final Context                           context;
 
-  public final Real                        α, β;
+  public final Real                              α, β;
 
-  public Function<Integer, RealPolynomial> F;
+  public final Function<Integer, RealPolynomial> F;
 
-  public static final String               Fdef = "n➔when(n=0,1,else,x*F(n-1))";
+  public static final String                     Fdef = "n➔when(n=0,1,else,x*F(n-1))";
 
   public HypergeometricFunctionSequence(int p, int q)
   {
     context = new Context(α = Real.newVector(p).setName("α"),
                           β = Real.newVector(q).setName("β"));
-    // F = Function.express(Integer.class, RealPolynomial.class, "F", Fdef,
-    // context);
+    F       = Function.express(Integer.class, RealPolynomial.class, "F", Fdef, context);
   }
 
   @Override
