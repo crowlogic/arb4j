44aba4f349611f5edeea10a5756eb0f7cdd73139
Stephen A. Crowley <crow@outsider.trading>
2024-01-28 05:34:16 -0600

    assert false : "TOOD: test variable indexing via UTF subscripted characters";

---
+++ src/test/java/arb/expressions/ExpressionTest.java
@@ -0,0 +1,12 @@
+package arb.expressions;
+
+import junit.framework.TestCase;
+
+public class ExpressionTest extends
+                            TestCase
+{
+  public void testVariableIndexedByASubscript()
+  {
+    assert false : "TOOD: test variable indexing via UTF subscripted characters";
+  }
+}
