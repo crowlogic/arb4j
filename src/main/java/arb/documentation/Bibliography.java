744e49c0a8c4e400970404bd67fc20e8f6181211
Stephen A. Crowley <crow@outsider.trading>
2024-01-27 22:16:50 -0600

    @Book{skewOrthogonalPolynomialsAndRandomMatrixTheory,  author = {Saugata Ghosh},  title = {Skew-Orthogonal Polynomials and Random Matrix Theory},  year = {2009},  publisher = {American Mathematical Society},  series = {CRM Monograph Series},  volume = {28}}

--- src/main/java/arb/documentation/Bibliography.java
+++ src/main/java/arb/documentation/Bibliography.java
@@ -8,48 +8,55 @@ package arb.documentation;
 public class Bibliography extends
                           AbstractBibliography
 {
-  public Book    randomMatrices                        = new Book("Madan Lal Mehta",
-                                                                  "Random Matrices",
-                                                                  "2004").setEdition("3rd")
-                                                                         .setSeries("Pure and Applied Mathematics")
-                                                                         .setVolume("142");
 
-  public Book    functionalAnalysisYosida              = new Book("吉田 耕作(Kōsaku Yosida)",
-                                                                  "Functional Analysis",
-                                                                  "1995").setPublisher("Springer Berlin Heidelberg")
-                                                                         .setSeries("Classics in Mathematics")
-                                                                         .setEdition("Reprint of the 1980 Edition");
+  public Book    skewOrthogonalPolynomialsAndRandomMatrixTheory = new Book("Saugata Ghosh",
+                                                                           "Skew-Orthogonal Polynomials and Random Matrix Theory",
+                                                                           "2009").setSeries("CRM Monograph Series")
+                                                                                  .setVolume("28")
+                                                                                  .setPublisher("American Mathematical Society");
 
-  public Book    linearTransformationsInHilbertSpace   = new Book("Marshall Harvey Stone",
-                                                                  "Linear Transformations in Hilbert Space",
-                                                                  "1932").setEdition("4th Printing (1951)")
-                                                                         .setVolume("XV")
-                                                                         .setSeries("Colloquium Publications")
-                                                                         .setPublisher("American Mathematical Society")
-                                                                         .setAddress("501 West 116th Street");
+  public Book    randomMatrices                                 = new Book("Madan Lal Mehta",
+                                                                           "Random Matrices",
+                                                                           "2004").setEdition("3rd")
+                                                                                  .setSeries("Pure and Applied Mathematics")
+                                                                                  .setVolume("142");
 
-  public Article orthogonalPolynomialFourierTransforms = new Article("Dixit, Atul and Jiu, Lin and Moll, Victor H and Vignat, Christophe",
-                                                                     "The finite Fourier transform of classical polynomials",
-                                                                     "2015",
-                                                                     "Journal of the Australian Mathematical Society",
-                                                                     "98",
-                                                                     "145--160").setPublisher("Cambridge University Press");
+  public Book    functionalAnalysisYosida                       = new Book("吉田 耕作(Kōsaku Yosida)",
+                                                                           "Functional Analysis",
+                                                                           "1995").setPublisher("Springer Berlin Heidelberg")
+                                                                                  .setSeries("Classics in Mathematics")
+                                                                                  .setEdition("Reprint of the 1980 Edition");
 
-  public Book    functionalAnalysisRieszNagy           = new Book("Frigyes Riesz and Béla Sz.-Nagy",
-                                                                  "Functional Analysis",
-                                                                  "2012").setPublisher("Dover Publications")
-                                                                         .setSeries("Dover Books on Mathematics");
+  public Book    linearTransformationsInHilbertSpace            = new Book("Marshall Harvey Stone",
+                                                                           "Linear Transformations in Hilbert Space",
+                                                                           "1932").setEdition("4th Printing (1951)")
+                                                                                  .setVolume("XV")
+                                                                                  .setSeries("Colloquium Publications")
+                                                                                  .setPublisher("American Mathematical Society")
+                                                                                  .setAddress("501 West 116th Street");
 
-  public Book    stochasticFiniteElements              = new Book("Ghanem, Roger G. and Spanos, Pol D.",
-                                                                  "Stochastic finite elements: a spectral approach",
-                                                                  "1991").setPublisher("Springer-Verlag")
-                                                                         .setAddress("Berlin, Heidelberg");
+  public Article orthogonalPolynomialFourierTransforms          = new Article("Dixit, Atul and Jiu, Lin and Moll, Victor H and Vignat, Christophe",
+                                                                              "The finite Fourier transform of classical polynomials",
+                                                                              "2015",
+                                                                              "Journal of the Australian Mathematical Society",
+                                                                              "98",
+                                                                              "145--160").setPublisher("Cambridge University Press");
 
-  public Book    stochasticProcessInferenceTheory      = new Book("Malempati M. Rao",
-                                                                  "Stochastic Processes: Inference Theory",
-                                                                  "2014").setPublisher("Springer")
-                                                                         .setEdition("2nd")
-                                                                         .setSeries("Springer Monographs in Mathematics");
+  public Book    functionalAnalysisRieszNagy                    = new Book("Frigyes Riesz and Béla Sz.-Nagy",
+                                                                           "Functional Analysis",
+                                                                           "2012").setPublisher("Dover Publications")
+                                                                                  .setSeries("Dover Books on Mathematics");
+
+  public Book    stochasticFiniteElements                       = new Book("Ghanem, Roger G. and Spanos, Pol D.",
+                                                                           "Stochastic finite elements: a spectral approach",
+                                                                           "1991").setPublisher("Springer-Verlag")
+                                                                                  .setAddress("Berlin, Heidelberg");
+
+  public Book    stochasticProcessInferenceTheory               = new Book("Malempati M. Rao",
+                                                                           "Stochastic Processes: Inference Theory",
+                                                                           "2014").setPublisher("Springer")
+                                                                                  .setEdition("2nd")
+                                                                                  .setSeries("Springer Monographs in Mathematics");
 
   public static void main(String args[])
   {
