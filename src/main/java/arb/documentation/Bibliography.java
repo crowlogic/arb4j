e8ca99391644e7ebce4a608385ef98c75033cd36
Stephen A. Crowley <crow@outsider.trading>
2024-01-30 18:57:15 -0600

    sort

--- src/main/java/arb/documentation/Bibliography.java
+++ src/main/java/arb/documentation/Bibliography.java
@@ -8,32 +8,10 @@ package arb.documentation;
 public class Bibliography extends
                           AbstractBibliography
 {
-  public static Book    stationaryAndRelatedProcesses      = new Book("Stationary and Related Processes: Sample Function Properties and Their Applications",
-                                                                      "Harald Cramér and M.R. Leadbetter",
-                                                                      "1967").setSeries("Wiley Series in Probability and Mathematical Statistics");
-
-  public static Book    stochasticQuantumMechanicalMethods = new Book("Stochastic Methods in Quantum Mechanics",
-                                                                      "Stanley P. Gudder",
-                                                                      "1979").setSeries("North Holland Series in Probability and Applied Mathematics")
-                                                                             .setPublisher("Elsevier North Holland");
-
-  public static Book    stationaryCorrelationFunctions     = new Book("Correlation Theory of Stationary and Related Random Functions",
-                                                                      "Yaglom, A.M.",
-                                                                      "1987").setSeries("Applied Probability")
-                                                                             .setPublisher("Springer New York")
-                                                                             .setVolume("I: Basic Results");
-
-  public static Book    skewOrthogonalRandomMatrices       = new Book("Saugata Ghosh",
-                                                                      "Skew-Orthogonal Polynomials and Random Matrix Theory",
-                                                                      "2009").setSeries("CRM Monograph Series")
-                                                                             .setVolume("28")
-                                                                             .setPublisher("American Mathematical Society");
-
-  public static Book    randomMatrices                     = new Book("Madan Lal Mehta",
-                                                                      "Random Matrices",
-                                                                      "2004").setEdition("3rd")
-                                                                             .setSeries("Pure and Applied Mathematics")
-                                                                             .setVolume("142");
+  public static Book    functionalAnalysisRieszNagy        = new Book("Frigyes Riesz and Béla Sz.-Nagy",
+                                                                      "Functional Analysis",
+                                                                      "2012").setPublisher("Dover Publications")
+                                                                             .setSeries("Dover Books on Mathematics");
 
   public static Book    functionalAnalysisYosida           = new Book("吉田 耕作(Kōsaku Yosida)",
                                                                       "Functional Analysis",
@@ -56,10 +34,27 @@ public class Bibliography extends
                                                                          "98",
                                                                          "145--160").setPublisher("Cambridge University Press");
 
-  public static Book    functionalAnalysisRieszNagy        = new Book("Frigyes Riesz and Béla Sz.-Nagy",
-                                                                      "Functional Analysis",
-                                                                      "2012").setPublisher("Dover Publications")
-                                                                             .setSeries("Dover Books on Mathematics");
+  public static Book    randomMatrices                     = new Book("Madan Lal Mehta",
+                                                                      "Random Matrices",
+                                                                      "2004").setEdition("3rd")
+                                                                             .setSeries("Pure and Applied Mathematics")
+                                                                             .setVolume("142");
+
+  public static Book    skewOrthogonalRandomMatrices       = new Book("Saugata Ghosh",
+                                                                      "Skew-Orthogonal Polynomials and Random Matrix Theory",
+                                                                      "2009").setSeries("CRM Monograph Series")
+                                                                             .setVolume("28")
+                                                                             .setPublisher("American Mathematical Society");
+
+  public static Book    stationaryAndRelatedProcesses      = new Book("Stationary and Related Processes: Sample Function Properties and Their Applications",
+                                                                      "Harald Cramér and M.R. Leadbetter",
+                                                                      "1967").setSeries("Wiley Series in Probability and Mathematical Statistics");
+
+  public static Book    stationaryCorrelationFunctions     = new Book("Correlation Theory of Stationary and Related Random Functions",
+                                                                      "Yaglom, A.M.",
+                                                                      "1987").setSeries("Applied Probability")
+                                                                             .setPublisher("Springer New York")
+                                                                             .setVolume("I: Basic Results");
 
   public static Book    stochasticFiniteElements           = new Book("Ghanem, Roger G. and Spanos, Pol D.",
                                                                       "Stochastic finite elements: a spectral approach",
@@ -71,6 +66,11 @@ public class Bibliography extends
                                                                       "2014").setPublisher("Springer")
                                                                              .setEdition("2nd")
                                                                              .setSeries("Springer Monographs in Mathematics");
+
+  public static Book    stochasticQuantumMechanicalMethods = new Book("Stochastic Methods in Quantum Mechanics",
+                                                                      "Stanley P. Gudder",
+                                                                      "1979").setSeries("North Holland Series in Probability and Applied Mathematics")
+                                                                             .setPublisher("Elsevier North Holland");
 
   public static void main(String args[])
   {
