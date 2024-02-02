c0f4d4fd8b0c96cc18e1bba93bad4f5a70e997b0
Stephen A. Crowley <stephencrowley214@gmail.com>
2024-02-01 21:22:03 -0600

    * TODO: use this whilst implementing https://github.com/crowlogic/arb4j/issues/287

---
+++ src/main/java/arb/algebra/CommutativeRing.java
@@ -0,0 +1,20 @@
+package arb.algebra;
+
+/**
+ * A commutative ring is a ring R that satisfies the additional axiom that
+ *  
+ * <pre>
+ * 
+ * ab = ba ∀a,b∈R
+ * 
+ * </pre>
+ * 
+ * TODO: use this whilst implementing https://github.com/crowlogic/arb4j/issues/287
+ * 
+ * @param <X>
+ */
+public interface CommutativeRing<X> extends
+                                Ring<X>
+{
+
+}
