9889c114b61d57e3e1a1cdec1ce31ef59f0c8c63
Stephen A. Crowley <stephencrowley214@gmail.com>
2024-02-01 21:23:16 -0600

    expression compiler: implement reorderable operands based on their commutativity(permutability) to facilitate memoization and automatic complexity reduction #287

--- src/main/java/arb/algebra/CommutativeRing.java
+++ src/main/java/arb/algebra/CommutativeRing.java
@@ -2,14 +2,17 @@ package arb.algebra;
 
 /**
  * A commutative ring is a ring R that satisfies the additional axiom that
- *  
+ * 
  * <pre>
  * 
- * ab = ba ∀a,b∈R
+ * ab = ba ∀a,b∈ℝ
  * 
  * </pre>
  * 
- * TODO: use this whilst implementing https://github.com/crowlogic/arb4j/issues/287
+ * TODO: use this whilst implementing
+ * https://github.com/crowlogic/arb4j/issues/287 - expression compiler:
+ * implement reorderable operands based on their commutativity(permutability) to
+ * facilitate memoization and automatic complexity reduction #287
  * 
  * @param <X>
  */
