12d9f0748daf7d59aebaac6b092ca67f32fef96a
Stephen A. Crowley <stephencrowley214@gmail.com>
2024-02-04 21:36:37 -0600

    todo: swinging factorials, https://github.com/crowlogic/arb4j/issues/319

--- F.java
+++ F.java
@@ -1,103 +1,53 @@
 import arb.Integer;
 import arb.Real;
-import arb.RealPolynomial;
 import arb.functions.Function;
 
-public class F implements Function<Integer, RealPolynomial> {
-   Integer c1 = new Integer("0");
-   Integer c2 = new Integer("1");
-   public Integer p;
-   public Integer q;
+public class F implements Function<Integer, Real> {
+   private boolean isInitialized;
+   Integer c1;
    public Real α;
    public Real β;
-   public Integer ℤ1 = new Integer();
-   public RealPolynomial r̅1 = new RealPolynomial();
-   public RealPolynomial r̅2 = new RealPolynomial();
-   public Real ℝ1 = new Real();
-   public Integer ℤ2 = new Integer();
-   public Real ℝ2 = new Real();
-   public RealPolynomial r̅3 = new RealPolynomial();
-   public Integer ℤ3 = new Integer();
-   public Integer ℤ4 = new Integer();
-   public Real ℝ3 = new Real();
-   public Integer ℤ5 = new Integer();
-   public Real ℝ4 = new Real();
-   public Real ℝ5 = new Real();
-   public F F;
+   public Real ℝ1;
+   public Integer ℤ1;
+   public Real ℝ2;
+   public Real ℝ3;
+   public Real ℝ4;
+   public final C C = new C();
 
-   public RealPolynomial evaluate(Integer var1, int var2, int var3, RealPolynomial result) {
-      return switch(in.getSignedValue()) {
-         case 0 -> result.set(c2);
-         default -> {
-            RealPolynomial result = result.identity();
-            if (result.F == null) {
-               result.F = new F(result);
-            }
+   public Real evaluate(Integer in, int order, int bits, Real result) {
+      if (!isInitialized) {
+         initialize();
+      }
 
-            RealPolynomial var5 = result.mul(result.F.evaluate(in.sub(result.c2, bits, result.ℤ1), order, bits, result.r̅1), bits, result.r̅2);
-            Real var10001 = result.ℝ1;
-            result.ℝ1.one();
-            result.k.set(result.startIndex);
-
-            while(result.k.compareTo(result.endIndex) <= 0) {
-               result.α.mul(result.identity().risingFactorial((var5).sub(result.c2, bits, result.ℤ2), bits, result.ℝ2), (int)var5, result.ℝ1);
-               result.k.increment();
-            }
-
-            RealPolynomial var6 = var5.mul(var10001, bits, result.r̅3);
-            Integer var7 = result.set((var5).sub(result.c2, bits, result.ℤ3).factorial(bits, result.ℤ4));
-            Real var10002 = result.ℝ3;
-            result.ℝ3.one();
-            result.k.set(result.startIndex);
-
-            while(result.k.compareTo(result.endIndex) <= 0) {
-               result.β
-                  .mul(result.identity().risingFactorial((var5).sub(result.c2, (int)var7, result.ℤ5), (int)var7, result.ℝ4), (int)var5, result.ℝ3);
-               result.k.increment();
-            }
-
-            yield var6.div(var7.mul(var10002, (int)var7, result.ℝ5), (int)var7, result);
-         }
-      };
+      return (C.evaluate(ℝ1.set(in.sub(c1, bits, ℤ1)), order, bits, ℝ2))
+         .mul(C.evaluate(ℝ3.set(in), order, bits, ℝ4), bits, result);
    }
 
    public F() {
+      c1 = new Integer("1");
+      ℝ1 = new Real();
+      ℤ1 = new Integer();
+      ℝ2 = new Real();
+      ℝ3 = new Real();
+      ℝ4 = new Real();
    }
 
-   public F(F var1) {
-      this();
-      if (var1.p == null) {
-         throw new AssertionError("p is null");
-      } else if (var1.α == null) {
-         throw new AssertionError("α is null");
-      } else if (var1.q == null) {
-         throw new AssertionError("q is null");
-      } else if (var1.β == null) {
-         throw new AssertionError("β is null");
+   public void initialize() {
+      if (isInitialized) {
+         throw new AssertionError("Already initialized");
       } else {
-         p = var1.p;
-         α = var1.α;
-         q = var1.q;
-         β = var1.β;
+         C.α = α;
+         C.β = β;
+         isInitialized = true;
       }
    }
 
    public void close() {
       c1.close();
-      c2.close();
-      ℤ1.close();
-      r̅1.close();
-      r̅2.close();
       ℝ1.close();
-      ℤ2.close();
+      ℤ1.close();
       ℝ2.close();
-      r̅3.close();
-      ℤ3.close();
-      ℤ4.close();
       ℝ3.close();
-      ℤ5.close();
       ℝ4.close();
-      ℝ5.close();
-      F.close();
    }
 }
---
+++ src/main/java/arb/expressions/nodes/binary/SwingingFactorial.java
@@ -0,0 +1,32 @@
+package arb.expressions.nodes.binary;
+
+import arb.expressions.Expression;
+import arb.expressions.nodes.Node;
+import arb.functions.Function;
+
+/**
+ *  
+ * @param <D>
+ * @param <R>
+ * @param <F>
+ */
+public class SwingingFactorial<D, R, F extends Function<D, R>> extends
+                              BinaryOperation<D, R, F>
+{
+
+  public SwingingFactorial(Expression<D, R, F> expression, Node<D, R, F> left, String operation, Node<D, R, F> right)
+  {
+    super(expression,
+          left,
+          operation,
+          right);
+  }
+
+  @Override
+  public String typeset()
+  {
+    assert false : "TODO: implement https://github.com/crowlogic/arb4j/issues/319";
+    return "TODO";
+  }
+
+}
