e16f1c5a9a2fae6b2db5d4607cebd38ef204eff2
Leo August Pochhammer <ancient@hist.ory>
2024-02-29 03:09:59 -0600

    unit tests

--- src/test/java/arb/expressions/nodes/nary/ProductTest.java
+++ src/test/java/arb/expressions/nodes/nary/ProductTest.java
@@ -18,32 +18,30 @@ import junit.framework.TestCase;
 public class ProductTest extends
                          TestCase
 {
-  public static void testProductNumerator()
-
+  /**
+   * In the theory of special functions (in particular the hypergeometric
+   * function) and in the standard reference work Abramowitz and Stegun, the
+   * Pochhammer symbol (x)n is used to represent the rising factorial.[6][7]
+   */
+  @SuppressWarnings("resource")
+  public static void testProductOfPochhammerSymbols()
   {
     Integer p;
-    Integer q;
     Real    α;
-    Real    β;
-    Context context = new Context(p = new Integer(3).setName("p"),
-                                  q = new Integer(1).setName("q"),
-                                  α = Real.newVector(p.getSignedValue()).setName("α"),
-                                  β = Real.newVector(q.getSignedValue()).setName("β"));
+    Context context = new Context(p = new Integer(4).setName("p"),
+                                  α = Real.newVector(p.getSignedValue()).setName("α"));
 
-    α.set(1.0, 2.0, 3.0);
-    β.set(4.0);
+    α.set(1.0, 2.0, 3.0, 4.0);
 
-    Function<Integer, Real> ratio = Function.express(Integer.class,
-                                                     Real.class,
-                                                     "F",
-                                                     "n➔∏α[k]₍ₙ₋₁₎{k=1…p}",
-                                                     context);
-    Function<Integer, Real> val   = ratio;
+    Function<Integer, Real> numer = Function.express(Integer.class, Real.class, "F", "n➔∏α[k]₍ₙ₋₁₎{k=1…p}", context);
+
     Integer                 in    = new Integer(2);
-    val.evaluate(in, 128, new Real());
-    System.out.format("numerator ratio(%s)=%s\n", in, val);
+    Real                    val   = numer.evaluate(in, 128, new Real());
+    assertEquals(24.0, val.doubleValue());
+    numer.evaluate(in.set(3), 128, val);
+    assertEquals(2880.0, val.doubleValue());
   }
-  
+
   public static void testRatioOfProducts()
   {
     Integer p;
