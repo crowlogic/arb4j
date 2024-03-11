877ecfcb982dd30cba203e8fb9044904fdc2c0f7
Stephen Crowley <stephencrowley214@gmail.com>
2024-03-10 20:28:28 -0500

    start inference package

---
+++ src/main/java/arb/inference/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * modeled after
+ * {@link arb.documentation.Bibliography#stochasticProcessInferenceTheory}
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+package arb.inference;
+
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
