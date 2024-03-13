4926353bcbbf2366cc186f864ce4eb57ad733a6b
Stephen Crowley <stephencrowley214@gmail.com>
2024-03-13 15:55:30 -0500

    a

--- src/main/java/arb/expressions/package-info.java
+++ src/main/java/arb/expressions/package-info.java
@@ -6,19 +6,28 @@
  * 
  * <p>
  * The package primarily revolves around the {@link arb.expressions.Expression}
- * class which implements the {@link arb.functions.real.RealFunction} interface.
- * It uses an expression tree to represent mathematical expressions, with each
- * node in the tree being an instance of {@link arb.expressions.nodes.Node}.
- * This class structure allows for parsing and manipulating complex expressions,
- * with support for constants, variables, binary operations, unary operations,
- * and function calls.
+ * class which generates {@link arb.functions.Function} implementations
+ * on-the-fly very quickly by using an expression tree to parse String
+ * representations of formulae with each node in the tree being an instance of
+ * {@link arb.expressions.nodes.Node}. This flexible structure allows for
+ * parsing and and executing many expressions, with support for
+ * {@link arb.expressions.nodes.LiteralConstant},
+ * {@link arb.expressions.nodes.Variable}s,
+ * {@link arb.expressions.nodes.binary.BinaryOperation}s, and
+ * {@link arb.expressions.nodes.unary.UnaryOperation}s such as
+ * {@link arb.expressions.nodes.unary.When} and the related
+ * {@link arb.expressions.nodes.Else} node as well as
+ * {@link arb.expressions.nodes.nary.NAryOperation}s like
+ * {@link arb.expressions.nodes.nary.Product} and
+ * {@link arb.expressions.nodes.nary.Sum}
  *
  * <p>
  * The {@link arb.expressions.Expression} class uses the objectweb ASM library
  * to generate bytecode for the mathematical expression it represents. It
  * dynamically creates a class that implements the
- * {@link arb.functions.real.RealFunction} interface, with each instance of the
- * class representing a specific mathematical expression.
+ * {@link arb.functions.Function} interface or any interface that extends it,
+ * with each instance of the class representing a specific mathematical
+ * expression.
  *
  * <p>
  * The {@link arb.expressions.Expression} class also provides methods for
@@ -27,23 +36,10 @@
  * expressions they represent.
  *
  * <p>
- * Although the current implementation only supports the
- * {@link arb.functions.real.RealFunction} interface, future updates will add
- * support for the ComplexFunction interface, extending the functionality of
- * this package to handle complex number expressions.
- *
- * <p>
  * The package also includes a suite of unit tests to ensure the correctness of
  * the expression parsing, transformation, evaluation, and bytecode generation
  * features.
- * 
- * <pre>
- * arb4j is made available under the terms of the Business Source License™ v1.1
- * ©2024 which can be found in the root directory of this project in a file
- * named License.pdf, License.txt, or License.tm which are the pdf, text, and
- * TeXmacs formatted versions of the same document respectively.
- * </pre>
- * 
+ *
  * @author ©2024 Stephen Crowley
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
  *      {@link TheArb4jLibrary}
