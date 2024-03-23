93f2bd4fc2450aa291f5fd51bef4caf4a580a266
Stephen Crowley <stephencrowley214@gmail.com>
2024-03-22 20:11:02 -0500

    todo: finish Wigner function implementation W(x,p)= ∫(ψ^(x+y)*ψ(x-y)*e^(2*i*p*y/ℏ)*d*y){y=-∞..∞}/(π*ℏ)<br>

---
+++ src/main/java/arb/functions/complex/WignerFunction.java
@@ -0,0 +1,28 @@
+package arb.functions.complex;
+
+import arb.Complex;
+
+/**
+ * W(x,p)= ∫(ψ^(x+y)*ψ(x-y)*e^(2*i*p*y/ℏ)*d*y){y=-∞..∞}/(π*ℏ)<br>
+ * <br>
+ * 
+ * 
+ * where ℏ is the normalizing constant equivalent to the Fourier transform pair
+ * or definition of units. The generalization of this is the Wigner–Weyl
+ * transform which is fun :-)
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class WignerFunction<F extends ComplexFunction> implements
+                           ComplexFunction
+{
+
+  @Override
+  public Complex evaluate(Complex t, int order, int bits, Complex res)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+}
