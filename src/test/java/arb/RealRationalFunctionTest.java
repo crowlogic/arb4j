3409ef175af03b71d1eccc98c4df10e3c9cc0558
Stephen Crowley <stephenrowley214@gmail.com>
2024-07-09 02:07:09 -0500

    The set of rational functions over a field K is a field, the field of fractions of the ring of the polynomial functions over K.

--- src/test/java/arb/RealRationalFunctionTest.java
+++ src/test/java/arb/RealRationalFunctionTest.java
@@ -3,7 +3,9 @@ package arb;
 import junit.framework.TestCase;
 
 /**
- *
+ * The set of rational functions over a field K is a field, the field of
+ * fractions of the ring of the polynomial functions over K.
+ * 
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
  *      {@link TheArb4jLibrary}
  */
@@ -12,12 +14,11 @@ public class RealRationalFunctionTest
                                       TestCase
 {
 
- public void testAdd()
- {
-   RealRationalFunction f = new RealRationalFunction();
-   RealRationalFunction g = new RealRationalFunction();
-   
- }
-  
+  public void testAdd()
+  {
+    RealRationalFunction f = new RealRationalFunction();
+    RealRationalFunction g = new RealRationalFunction();
+
+  }
 
 }
