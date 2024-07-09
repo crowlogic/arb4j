c76f21610c34dd85c1a4046cfbd84e3e1e0a19a8
Stephen Crowley <stephenrowley214@gmail.com>
2024-07-09 02:00:44 -0500

    https://github.com/crowlogic/arb4j/issues/440  implement RationalFunction class #440

---
+++ src/main/java/arb/RationalFunction.java
@@ -0,0 +1,137 @@
+package arb;
+
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+
+/**
+ *
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class RationalFunction
+                               implements
+                              Field<RationalFunction>
+{
+
+  private int bits;
+
+  @Override
+  public int compareTo(RationalFunction o)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return 0;
+  }
+
+  @Override
+  public RationalFunction add(RationalFunction element, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public int bits()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return bits;
+  }
+
+  @Override
+  public int dim()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return 1;
+  }
+
+  @Override
+  public RationalFunction div(int j, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction div(RationalFunction j, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction mul(int x, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction mul(RationalFunction x, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction newFieldElement()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction set(RationalFunction value)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction sub(RationalFunction element, int prec, RationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public <N extends Named> N setName(String name)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction additiveIdentity()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction multiplicativeIdentity()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction get(int index)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public String getName()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RationalFunction zero()
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+}
---
+++ src/main/java/arb/RealRationalFunction.java
@@ -0,0 +1,96 @@
+package arb;
+
+import java.util.Objects;
+
+import arb.algebra.Ring;
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+
+/**
+ *
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class RealRationalFunction implements Ring<RealRationalFunction>, Named, Comparable<RealRationalFunction>
+
+{
+  public String         name;
+
+  public RealPolynomial value;
+
+  public RealPolynomial remainder;
+
+  public RealPolynomial divisor;
+
+  @Override
+  public RealRationalFunction mul(RealRationalFunction operand, int prec, RealRationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public RealRationalFunction setName(String name)
+  {
+    this.name = name;
+    return this;
+  }
+
+  @Override
+  public RealRationalFunction add(RealRationalFunction addend, int bits, RealRationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RealRationalFunction sub(RealRationalFunction subtrahend, int bits, RealRationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public RealRationalFunction div(RealRationalFunction x, int prec, RealRationalFunction result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public String getName()
+  {
+    return name;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (!(obj instanceof RealRationalFunction))
+    {
+      return false;
+    }
+    RealRationalFunction that = (RealRationalFunction) obj;
+    return Objects.equals(value, that.value) && Objects.equals(remainder, that.remainder)
+                  && Objects.equals(divisor, that.divisor);
+  }
+
+  @Override
+  public int compareTo(RealRationalFunction o)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return 0;
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format("RealRationalFunction[name=%s, value=%s, remainder=%s, divisor=%s]",
+                         name,
+                         value,
+                         remainder,
+                         divisor);
+  }
+
+}
