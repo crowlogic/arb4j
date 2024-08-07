39461ccdf15b5c86eefb6b217208bea3119dfe01
Stephen Crowley <stephencrowley214@gmail.com>
2024-08-07 17:03:32 -0500

    ComplexFraction to be used by ComplexRationalFunction which will have a real and imagary IntegerPolynomial for the numer and denom

---
+++ src/main/java/arb/ComplexFraction.java
@@ -0,0 +1,143 @@
+package arb;
+
+import java.lang.foreign.Arena;
+
+/**
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class ComplexFraction implements Field<ComplexFraction>,AutoCloseable
+{
+  @Override
+  public void close()
+  {
+   if ( arena != null )
+   {
+     arena.close();
+   }
+   arena = null;
+   realPart = imaginaryPart = parts = null;
+  }
+
+  Arena arena ;
+  
+  Fraction parts;
+
+  Fraction realPart;
+
+  Fraction imaginaryPart;
+  
+  public ComplexFraction()
+  {
+    arena = Arena.ofShared();
+    parts = Fraction.newVector(arena, 2);
+    realPart = parts.get(0);
+    imaginaryPart = parts.get(1);
+  }
+
+  @Override
+  public ComplexFraction additiveIdentity()
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction multiplicativeIdentity()
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction add(ComplexFraction element, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public int bits()
+  {
+    assert false : "TODO";
+    return 128;
+  }
+
+  @Override
+  public int dim()
+  {
+    assert false : "TODO";
+    return 1;
+  }
+
+  @Override
+  public ComplexFraction div(int j, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction div(ComplexFraction j, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction get(int index)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public String getName()
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction mul(int x, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction mul(ComplexFraction x, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction newFieldElement()
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction set(ComplexFraction value)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction sub(ComplexFraction element, int prec, ComplexFraction result)
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+  @Override
+  public ComplexFraction zero()
+  {
+    assert false : "TODO";
+    return null;
+  }
+
+}
---
+++ src/test/java/arb/ComplexFractionTest.java
@@ -0,0 +1,15 @@
+package arb;
+
+import junit.framework.TestCase;
+
+/**
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class ComplexFractionTest
+                                 extends
+                                 TestCase
+{
+
+}
