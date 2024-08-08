7fd14f0f01fa642a7cc71a4f1aca2162f0c1306d
Stephen Crowley <stephenrowley214@gmail.com>
2024-08-08 14:21:40 -0500

    a complex fraction is the quotient of a pair of gaussian integers

---
+++ src/main/java/arb/GaussianInteger.java
@@ -0,0 +1,52 @@
+package arb;
+
+import arb.algebra.Ring;
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+
+/**
+ * A Gaussian integer is a {@link Complex} number whose real and imaginary parts are
+ * both integers. The Gaussian integers, with ordinary addition and
+ * multiplication of complex numbers, form an integral domain, usually written
+ * as ℤ[i] or ℤ[i].
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class GaussianInteger implements Ring<GaussianInteger>
+{
+  Integer parts = Integer.newVector(2);
+  
+  Integer realPart = parts.get(0);
+  
+  Integer imaginarypart = parts.get(1);
+  
+  @Override
+  public GaussianInteger mul(GaussianInteger operand, int prec, GaussianInteger result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public GaussianInteger add(GaussianInteger addend, int bits, GaussianInteger result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public GaussianInteger sub(GaussianInteger subtrahend, int bits, GaussianInteger result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+  @Override
+  public GaussianInteger div(GaussianInteger x, int prec, GaussianInteger result)
+  {
+    assert false : "TODO: Auto-generated method stub";
+    return null;
+  }
+
+}
--- src/test/java/arb/ComplexFractionTest.java
+++ src/test/java/arb/ComplexFractionTest.java
@@ -11,5 +11,16 @@ public class ComplexFractionTest
                                  extends
                                  TestCase
 {
+  @SuppressWarnings("resource")
+  public void testSet()
+  {
+    ComplexFraction a = new ComplexFraction().set("7/8");
+    ComplexFraction b = new ComplexFraction().set("1/4");
+    assertFalse(a.equals(b));
+    b.set(a);
+    assertTrue(a.equals(b));
+    assertTrue(b.getNumerator().equals(a.getNumerator()));
+    assertTrue(b.getDenominator().equals(a.getDenominator()));
 
+  }
 }
