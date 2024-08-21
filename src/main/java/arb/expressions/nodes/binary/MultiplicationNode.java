97d56cc74015392bca59894a0a46079a93c41651
Stephen Crowley <stephencrowley214@gmail.com>
2024-08-20 21:09:06 -0500

    hope i dont need these damn things

--- src/main/java/arb/expressions/nodes/binary/MultiplicationNode.java
+++ src/main/java/arb/expressions/nodes/binary/MultiplicationNode.java
@@ -2,6 +2,7 @@ package arb.expressions.nodes.binary;
 
 import static java.lang.String.format;
 
+import arb.Quaternion;
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
 import arb.expressions.Expression;
@@ -31,10 +32,13 @@ public class MultiplicationNode<D, R, F extends Function<? extends D, ? extends 
     return format("%s \\cdot %s", left.typeset(), right.typeset());
   }
 
+  /**
+   * @return true if {@link Expression#coDomainType} is not a {@link Quaternion}
+   */
   @Override
   public boolean isCommutative()
   {
-    return true;
+    return !expression.coDomainType.equals(Quaternion.class);
   }
 
   @Override
