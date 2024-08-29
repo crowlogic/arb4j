21e695297e8e3aa2e8ff2141f547fdc23bfcc335
Stephen Crowley <stephencrowley214@gmail.com>
2024-08-29 00:41:03 -0500

    ...

--- src/test/java/arb/functions/rational/ComplexRationalFunctionTest.java
+++ src/test/java/arb/functions/rational/ComplexRationalFunctionTest.java
@@ -52,9 +52,7 @@ public class ComplexRationalFunctionTest extends
     var x    = R.evaluate(3, 128);
     var xToo = Rtoo.evaluate(3, 128);
     assertEquals("(-6*x^2+15)/(x^3) + 0i", x.toString());
-    assertEquals("the negative power of the complex rational function is not being calculated properly",
-                 "(-6*x^2+15)/(x^3) + 0i",
-                 xToo.toString());
+    assertEquals("(-6*x^2+15)/(x^3) + 0i", xToo.toString());
   }
 
   public void testComplexRationalLommelPolynomialsAsNullary()
@@ -66,7 +64,7 @@ public class ComplexRationalFunctionTest extends
           ComplexRationalNullaryFunction.express("v₍ₙ₎*(z/2)^(-n)*pFq([1⁄2-n/2,-n/2],[v,-n,1-v-n],-z²)",
                                                  context);
     var x       = R.evaluate();
-    System.out.println( "gr8=" + x.toString() );
+    System.out.println("gr8=" + x.toString());
     assertEquals("(-6*x^2+15)/(x^3) + 0i", x.toString());
   }
 
