06cc5ca35e419232b9b808ce7e252331de5852fa
Stephen Crowley <stephencrowley214@gmail.com>
2024-08-30 18:05:32 -0500

    testComplexNullaryFunctionSquareRootOfNegativeOne

---
+++ src/test/java/arb/functions/complex/ComplexFunctionTest.java
@@ -0,0 +1,28 @@
+package arb.functions.complex;
+
+import arb.Real;
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+import arb.expressions.Context;
+import arb.functions.real.RealNullaryFunction;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class ComplexFunctionTest extends
+                                 TestCase
+{
+
+  public static void testComplexNullaryFunctionSquareRootOfNegativeOne()
+  {
+    var context = new Context(RealNullaryFunction.express("1/2").evaluate(128, Real.named("v")));
+    var f       = ComplexNullaryFunction.express("sqrt(-1)*v", context);
+    var x       = f.evaluate(128);
+    System.out.println(f + "=" + x);
+
+  }
+
+}
