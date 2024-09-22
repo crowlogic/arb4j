ce42fefec33f812743eb2bffdc72a70221e3be3d
Stephen Crowley <stephencrowley214@gmail.com>
2024-09-22 17:10:25 -0500

    ...

--- src/test/java/arb/equations/EquationTest.java
+++ src/test/java/arb/equations/EquationTest.java
@@ -17,17 +17,17 @@ public class EquationTest
                           TestCase
 {
 
-  public static void testBesselFunctionOrthogonality()
-  {
-    try ( Real v = new Real("1.0",
-                            128);
-          Integer n = new Integer(3); Integer m = new Integer(2);)
-    {
-      Context      context       = new Context(v.setName("v"),
-                                               n.setName("n"),
-                                               m.setName("m"));
-      RealEquation orthogonality = new RealEquation("∫x->(J(v+2*n+1,x)*J(v+2*m+1,x)/x)dx∈(0,∞) = sin(π*n-π*m)/(2*π*(v+n+1+m)*(1-m))",
-                                                    context);
-    }
-  }
+//  public static void testBesselFunctionOrthogonality()
+//  {
+//    try ( Real v = new Real("1.0",
+//                            128);
+//          Integer n = new Integer(3); Integer m = new Integer(2);)
+//    {
+//      Context      context       = new Context(v.setName("v"),
+//                                               n.setName("n"),
+//                                               m.setName("m"));
+//      RealEquation orthogonality = new RealEquation("∫x->(J(v+2*n+1,x)*J(v+2*m+1,x)/x)dx∈(0,∞) = sin(π*n-π*m)/(2*π*(v+n+1+m)*(1-m))",
+//                                                    context);
+//    }
+//  }
 }
