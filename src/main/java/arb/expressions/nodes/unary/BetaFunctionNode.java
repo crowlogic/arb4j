1830cc4ff2cbc403b2b53c2ef43ce81c94232d19
Stephen Crowley <stephencrowley214@gmail.com>
2024-10-02 02:40:35 -0500

    https://github.com/crowlogic/arb4j/issues/510

---
+++ src/main/java/arb/expressions/nodes/unary/BetaFunctionNode.java
@@ -0,0 +1,36 @@
+package arb.expressions.nodes.unary;
+
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+import arb.expressions.Expression;
+import arb.expressions.nodes.Node;
+import arb.functions.Function;
+
+/**
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class BetaFunctionNode<D, C, F extends Function<? extends D, ? extends C>> extends
+                             FunctionCallNode<D, C, F>
+{
+
+  private Node<D, C, F> p;
+  private Node<D, C, F> q;
+
+  public BetaFunctionNode(Expression<D, C, F> expression)
+  {
+    super("B",
+          null,
+          expression);
+
+    p = expression.resolve();
+
+    expression.require(',');
+
+    q = expression.resolve();
+
+    expression.require(')');
+
+  }
+
+}
