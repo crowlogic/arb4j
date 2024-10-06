898305cd8b8631a1ebd6e6dbacb91a250af2f1ab
Stephen Crowley <stephencrowley214@gmail.com>
2024-10-06 15:59:35 -0500

    ...

--- src/main/java/arb/expressions/nodes/unary/BetaFunctionNode.java
+++ src/main/java/arb/expressions/nodes/unary/BetaFunctionNode.java
@@ -2,62 +2,53 @@ package arb.expressions.nodes.unary;
 
 import org.objectweb.asm.MethodVisitor;
 
-import arb.Named;
-import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
-import arb.documentation.TheArb4jLibrary;
 import arb.expressions.Context;
 import arb.expressions.Expression;
 import arb.expressions.nodes.Node;
 import arb.functions.Function;
 
-/**
- * @see BusinessSourceLicenseVersionOnePointOne © terms of the
- *      {@link TheArb4jLibrary}
- */
 public class BetaFunctionNode<D, C, F extends Function<? extends D, ? extends C>> extends
                              FunctionCallNode<D, C, F>
 {
 
-  private Node<D, C, F> x;
-  private Node<D, C, F> y;
+  private final Node<D, C, F>       x;
+  private final Node<D, C, F>       y;
+  private final Expression<D, C, F> definition;
 
   public BetaFunctionNode(Expression<D, C, F> expression)
   {
-    super("B",
+    super("Beta",
           null,
           expression);
 
     x = expression.resolve();
-
     expression.require(',');
-
     y = expression.resolve();
-
     expression.require(')');
 
-    xval       = ((Named) expression.newCoDomainInstance()).setName("x");
-    yval       = ((Named) expression.newCoDomainInstance()).setName("y");
-    context    = new Context((Named) xval,
-                             (Named) yval);
+    Context context = new Context();
+    context.registerVariable("x", expression.newCoDomainInstance());
+    context.registerVariable("y", expression.newCoDomainInstance());
+
     definition = Function.compile(expression.domainType,
                                   expression.coDomainType,
                                   expression.functionClass,
-                                  "(Γ(x)*Γ(y))/Γ(x+y)",
+                                  "Γ(x)*Γ(y)/Γ(x+y)",
                                   context);
   }
-
-  D                   xval;
-  D                   yval;
-
-  Context             context;
-
-  Expression<D, C, F> definition;
 
   @Override
   public MethodVisitor generate(MethodVisitor mv, Class<?> resultType)
   {
+    x.generate(mv, resultType);
+    expression.loadFieldOntoStack(mv, "x", resultType);
+    expression.putField(mv, "x", resultType);
+
+    y.generate(mv, resultType);
+    expression.loadFieldOntoStack(mv, "y", resultType);
+    expression.putField(mv, "y", resultType);
+
     definition.rootNode.generate(mv, resultType);
     return mv;
   }
-
 }
