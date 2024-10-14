e073453a6ff06a171e0410ea4a65ec6ea6ce451f
Stephen Crowley <stephencrowley214@gmail.com>
2024-10-14 03:42:13 -0500

    ...

--- src/main/java/arb/viz/WindowManager.java
+++ src/main/java/arb/viz/WindowManager.java
@@ -5,6 +5,8 @@ import java.io.PrintWriter;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 
 import arb.XDO;
@@ -15,6 +17,7 @@ import arb.expressions.nodes.Node;
 import arb.expressions.viz.EmacsKeybindingsEventHandler;
 import arb.utensils.Utensils;
 import javafx.application.Platform;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.skin.TableColumnHeader;
@@ -26,7 +29,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 
 /**
- * Restore the crippled toFront() functionality in Javafx that was put there
+ * Restore the completely broken toFront() functionality in Javafx that was put there
  * because microsoft is a flaming heap of dung
  * 
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
@@ -42,6 +45,59 @@ public class WindowManager
   static
   {
     Thread.setDefaultUncaughtExceptionHandler(WindowManager::showError);
+  }
+
+  public static final String EASIER_ON_THE_EYES_STYLESHEET = ".scroll-bar .thumb {\n"
+                                                             + "    -fx-background-color: #808080; /* Change this to your desired thumb color */\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".scroll-bar .increment-button, .scroll-bar .decrement-button {\n"
+                                                             + "    -fx-background-color: #a9a9a9; /* Change this to your desired button color */\n"
+                                                             + "}\n"
+                                                             + ".tree-table-view .column-resize-line {\n"
+                                                             + "    -fx-background-color: #ff0000; /* Change this to your desired color */\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".tree-table-view .tree-table-row-cell .tree-table-cell .text {\n"
+                                                             + "    -fx-font-size: 18px; /* This specifically targets TreeTableView cells */\n"
+                                                             + "    -fx-fill: #ffff00; /* Change this to your desired color */\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".root {\n"
+                                                             + "    -fx-font-weight: bold; /* Make font bold */\n"
+                                                             + "  -fx-font-size: 18px; /* Change this value to increase/decrease the font size */\n"
+                                                             + "  -fx-base: #1f273f;\n"
+                                                             + "  -fx-default-button: #7f878f;\n"
+                                                             + "  -fx-focus-color: #efefef;\n"
+                                                             + "  -fx-focused-text-base-color: ladder(-fx-selection-bar, -fx-light-text-color 45%, -fx-dark-text-color\n"
+                                                             + "    46%, -fx-dark-text-color 59%, -fx-mid-text-color 60%);\n"
+                                                             + "  -fx-focused-mark-color: -fx-focused-text-base-color;\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".text-input:focused {\n"
+                                                             + "  -fx-highlight-text-fill: ladder(-fx-highlight-fill, -fx-light-text-color 45%, -fx-dark-text-color\n"
+                                                             + "    46%, -fx-dark-text-color 59%, -fx-mid-text-color 60%);\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".chart-legend-item {\n"
+                                                             + "  -fx-font-size: 20px;\n"
+                                                             + "  font-size: 20px;\n"
+                                                             + "}\n"
+                                                             + "\n"
+                                                             + ".chart-datapoint-tooltip-label {\n"
+                                                             + "  -fx-font-size: 20px;\n"
+                                                             + "  font-size: 20px;}\n"
+                                                             + ".tree-table-row-cell { -fx-border-color: #CCCCCC; -fx-border-width: 0 0 1 0; }\n";
+
+  public static String convertStylesheetToDataURI(String CSS_CONTENT)
+  {
+    String encoded = Base64.getEncoder().encodeToString(CSS_CONTENT.getBytes(StandardCharsets.UTF_8));
+    return "data:text/css;base64," + encoded;
+  }
+
+  public static void setDarkStyle(Scene scene)
+  {
+    scene.getStylesheets().add(convertStylesheetToDataURI(EASIER_ON_THE_EYES_STYLESHEET));
   }
 
   private static XDO    xdo;
@@ -168,7 +224,7 @@ public class WindowManager
   }
 
   @SuppressWarnings("unchecked")
-  public static <Y,T extends IndexedCell<? extends Y>>
+  public static <Y, T extends IndexedCell<? extends Y>>
 
          VirtualFlow<T>
          getVirtualFlow(Control treeTableView2)
