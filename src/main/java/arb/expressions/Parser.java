cea9203540cf863d928eacbe2bbf887b3c6624de
Stephen Crowley <stephencrowley214@gmail.com>
2024-10-25 05:48:33 -0500

    more curect

--- src/main/java/arb/expressions/Parser.java
+++ src/main/java/arb/expressions/Parser.java
@@ -115,7 +115,7 @@ public class Parser
    * Extending on that, in order to display one and fifteen sixteenths, rather than one hundred 
    * fifteen sixteenths, Any zero-width, invisible character can be used to separate the "1" from 
    * the "15" in an expression such as "15⁄16"  because the rendering engine will declare the fraction 
-   * ended when it encounters an symbol such as U+2064 INVISIBLE PLUS, U+200C ZERO WIDTH NON-JOINER, 
+   * ended when it encounters a symbol such as U+2064 INVISIBLE PLUS, U+200C ZERO WIDTH NON-JOINER, 
    * or U+2060 WORD JOINER or other invisible character which produces the same result  
    * </pre>
    * 
