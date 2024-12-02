05180de8e3f157936d99d9abc2a4fded5d56071e
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-02 10:24:04 -0600

    Close #532

---
+++ src/main/java/arb/physics/particles/TensorBoson.java
@@ -0,0 +1,11 @@
+package arb.physics.particles;
+
+/**
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public interface TensorBoson extends
+                             Boson
+{
+
+}
