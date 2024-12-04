a79bcc555b522cfaa871157113db38a054a15778
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-03 21:04:22 -0600

    https://github.com/crowlogic/arb4j/issues/546

--- src/main/java/arb/functions/OrthogonalPolynomialExpansion.java
+++ src/main/java/arb/functions/OrthogonalPolynomialExpansion.java
@@ -1,14 +1,23 @@
 package arb.functions;
 
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
 import arb.functions.polynomials.orthogonal.OrthogonalPolynomialSequence;
 
 /**
+ * 
+ * @param <D>
+ * @param <C>
+ * @param <F>
+ * @param <O> the type of {@link OrthogonalPolynomialSequence} that will be used
+ *            to
+ * 
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
  *      {@link TheArb4jLibrary}
  */
 public interface OrthogonalPolynomialExpansion<D, C, F extends Function<? extends D, ? extends C>,
               O extends OrthogonalPolynomialSequence<? extends D, ? extends C>> extends
-                                              Expansion<D, C, F>
+                                              Expansion<D, C, OrthogonalPolynomialExpansion<D, C, F, O>>
 {
 
 }
