f8a35404c92cbf772fb1a68c6ddbe8bc8c1bdc62
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-11 01:23:00 -0600

    java.lang.NoSuchMethodError: 'arb.RealMatrix arb.Integer.add(arb.Integer, int, arb.RealMatrix)' 	at _2+3.evaluate(Unknown Source) 	at arb4j/arb.functions.Function.evaluate(Function.java:242)

--- src/main/java/arb/expressions/nodes/binary/BinaryOperationNode.java
+++ src/main/java/arb/expressions/nodes/binary/BinaryOperationNode.java
@@ -21,7 +21,6 @@ import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
 import arb.exceptions.CompilerException;
 import arb.expressions.Expression;
-import arb.expressions.nodes.LiteralConstantNode;
 import arb.expressions.nodes.Node;
 import arb.expressions.nodes.VariableNode;
 import arb.functions.Function;
