0f86ec9abf0bf21e2e4efbdbc9449803397da110
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-12 10:30:21 -0600

    add Lagueerr polynomial test

---
+++ src/test/java/arb/functions/polynomials/orthogonal/real/LaguerrePolynomialsTest.java
@@ -0,0 +1,18 @@
+package arb.functions.polynomials.orthogonal.real;
+
+import junit.framework.TestCase;
+
+/**
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public class LaguerrePolynomialsTest extends
+                                     TestCase
+{
+  public void testLaguerrePolynomials()
+  {
+    var L  = new LaguerrePolynomials();
+    var L2 = L.evaluate(2, 128);
+    assertEquals("0.5*x² - 2*x + 1", L2.toString());
+  }
+}
