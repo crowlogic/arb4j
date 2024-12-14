f39a4f721117e3a65bce34ad5de8b044e86fa57b
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-14 08:03:01 -0600

    https://github.com/crowlogic/arb4j/issues/547

--- src/main/java/arb/functions/real/P.java
+++ src/main/java/arb/functions/real/P.java
@@ -12,13 +12,6 @@ public class P implements
                AutoCloseable,
                Initializable
 {
-  public P()
-  {
-    super();
-    System.err.println("newP " + Utensils.stackTraceToString(new Throwable()));
-    System.err.flush();
-  }
-
   public static void main(String args[])
   {
     P P = new P();
@@ -31,38 +24,101 @@ public class P implements
     System.out.println(P + "(3)=" + P3);
   }
 
-  public boolean          isInitialized;
-  public final Integer    cℤ0002 = new Integer("1");
+  public A                A;
+
+  public B                B;
+  public C                C;
   public final Integer    cℤ0001 = new Integer("0");
+  public final Integer    cℤ0002 = new Integer("1");
   public final Integer    cℤ0003 = new Integer("2");
+  public E                E;
+  public boolean          isInitialized;
+  public P                P;
   public Fraction         vf0010 = new Fraction();
-  public RationalFunction vℚ0015 = new RationalFunction();
-  public Integer          vℤ0006 = new Integer();
-  public RationalFunction vℚ0016 = new RationalFunction();
+  public Fraction         vf0011 = new Fraction();
+  public Fraction         vf0012 = new Fraction();
   public RationalFunction vℚ0006 = new RationalFunction();
   public RationalFunction vℚ0007 = new RationalFunction();
   public RationalFunction vℚ0008 = new RationalFunction();
   public RationalFunction vℚ0009 = new RationalFunction();
-  public Integer          vℤ0005 = new Integer();
   public RationalFunction vℚ0010 = new RationalFunction();
   public RationalFunction vℚ0011 = new RationalFunction();
   public RationalFunction vℚ0012 = new RationalFunction();
   public RationalFunction vℚ0013 = new RationalFunction();
   public RationalFunction vℚ0014 = new RationalFunction();
-  public Fraction         vf0012 = new Fraction();
-  public Fraction         vf0011 = new Fraction();
-  public C                C;
-  public E                E;
-  public B                B;
-  public A                A;
-  public P                P;
+  public RationalFunction vℚ0015 = new RationalFunction();
+  public RationalFunction vℚ0016 = new RationalFunction();
+  public Integer          vℤ0005 = new Integer();
+  public Integer          vℤ0006 = new Integer();
   public Real             α;
   public Real             β;
+
+  public P()
+  {
+    super();
+    System.err.println("newP " + Utensils.stackTraceToString(new Throwable()));
+    System.err.flush();
+  }
+
+  @Override
+  public void close()
+  {
+    cℤ0002.close();
+    cℤ0001.close();
+    cℤ0003.close();
+    vf0010.close();
+    vℚ0015.close();
+    vℤ0006.close();
+    vℚ0016.close();
+    vℚ0006.close();
+    vℚ0007.close();
+    vℚ0008.close();
+    vℚ0009.close();
+    vℤ0005.close();
+    vℚ0010.close();
+    vℚ0011.close();
+    vℚ0012.close();
+    vℚ0013.close();
+    vℚ0014.close();
+    vf0012.close();
+    vf0011.close();
+    P.close();
+    A.close();
+    B.close();
+    C.close();
+    E.close();
+  }
 
   @Override
   public Class<RationalFunction> coDomainType()
   {
     return RationalFunction.class;
+  }
+
+  protected void constructReferencedFunctions()
+  {
+    if (A == null)
+    {
+      A = new A();
+    }
+
+    if (B == null)
+    {
+      B = new B();
+    }
+
+    if (C == null)
+    {
+      C = new C();
+    }
+
+    if (E == null)
+    {
+      E = new E();
+    }
+    P     = new P();
+    P.A   = A;
+    P.A.F = A.F = new F();
   }
 
   @Override
@@ -121,69 +177,39 @@ public class P implements
 
   protected void injectVariableReferences()
   {
-    A.α           = B.α = C.α = E.α = P.α = A.F.α = α;
-    A.β           = B.β = C.β = E.β = P.β = A.F.β = β;
-  }
-
-  protected void constructReferencedFunctions()
-  {
-    if (A == null)
-    {
-      A = new A();
-    }
-
-    if (B == null)
-    {
-      B = new B();
-    }
-
-    if (C == null)
-    {
-      C = new C();
-    }
-
-    if (E == null)
-    {
-      E = new E();
-    }
-    P   = new P();
-    P.A = A;
-    P.A.F = A.F = new F();
-  }
-
-  @Override
-  public void close()
-  {
-    cℤ0002.close();
-    cℤ0001.close();
-    cℤ0003.close();
-    vf0010.close();
-    vℚ0015.close();
-    vℤ0006.close();
-    vℚ0016.close();
-    vℚ0006.close();
-    vℚ0007.close();
-    vℚ0008.close();
-    vℚ0009.close();
-    vℤ0005.close();
-    vℚ0010.close();
-    vℚ0011.close();
-    vℚ0012.close();
-    vℚ0013.close();
-    vℚ0014.close();
-    vf0012.close();
-    vf0011.close();
-    P.close();
-    A.close();
-    B.close();
-    C.close();
-    E.close();
+    A.α = B.α = C.α = E.α = P.α = A.F.α = α;
+    A.β = B.β = C.β = E.β = P.β = A.F.β = β;
   }
 
   @Override
   public String toString()
   {
     return "P:n➔When[cases=[0=1, 1=((C(1)*x)-β+α)/2],default=((A(n)*P(n-1))-(B(n)*P(n-2)))/E(n)]";
+  }
+
+  public Typesettable intermediateStates()
+  {
+    return () -> String.format("P[cℤ0001=%s, cℤ0002=%s, cℤ0003=%s, vf0010=%s, vf0011=%s, vf0012=%s, vℚ0006=%s, vℚ0007=%s, vℚ0008=%s, vℚ0009=%s, vℚ0010=%s, vℚ0011=%s, vℚ0012=%s, vℚ0013=%s, vℚ0014=%s, vℚ0015=%s, vℚ0016=%s, vℤ0005=%s, vℤ0006=%s]",
+                               cℤ0001,
+                               cℤ0002,
+                               cℤ0003,
+                               vf0010,
+                               vf0011,
+                               vf0012,
+                               vℚ0006,
+                               vℚ0007,
+                               vℚ0008,
+                               vℚ0009,
+                               vℚ0010,
+                               vℚ0011,
+                               vℚ0012,
+                               vℚ0013,
+                               vℚ0014,
+                               vℚ0015,
+                               vℚ0016,
+                               vℤ0005,
+                               vℤ0006);
+
   }
 
   @Override
