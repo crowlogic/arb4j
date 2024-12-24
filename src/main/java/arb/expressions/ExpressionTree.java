882de9f5a3e52b736a3015604a1e6e91113f1521
Stephen Crowley <stephencrowley214@gmail.com>
2024-12-23 18:20:43 -0600

    https://github.com/crowlogic/arb4j/issues/560

--- src/main/java/arb/expressions/ExpressionTree.java
+++ src/main/java/arb/expressions/ExpressionTree.java
@@ -1,8 +1,12 @@
 package arb.expressions;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
@@ -19,8 +23,74 @@ import arb.utensils.text.trees.TreeModel;
  *      {@link TheArb4jLibrary}
  */
 public class ExpressionTree<D, R, F extends Function<? extends D, ? extends R>> implements
-                            TreeModel<Node<D, R, F>>
+                           TreeModel<Node<D, R, F>>
 {
+  /**
+   * Exports the expression tree to a DOT format file with field names as link
+   * labels
+   * 
+   * @param filePath the path where to save the .dot file
+   */
+  public void saveToDotFile(String filePath)
+  {
+    StringBuilder dot = new StringBuilder();
+    dot.append("digraph ExpressionTree {\n");
+    dot.append("  rankdir=TB;\n"); // Top to Bottom direction
+    dot.append("  node [shape=circle];\n\n");
+
+    // Use a queue for breadth-first traversal
+    Queue<Node<D, R, F>> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty())
+    {
+      Node<D, R, F> current = queue.poll();
+
+      // Add node definition
+      dot.append(String.format("  \"%s\" [label=\"%s\"];\n", current.toString(), current.toString()));
+
+      // Process all branches
+      List<Node<D, R, F>> branches = indexedBranches.get(current);
+      if (branches != null)
+      {
+        for (Node<D, R, F> child : branches)
+        {
+          queue.add(child);
+
+          // Get the field name if available
+          String fieldName = "";
+          if (child instanceof Node arbNode)
+          {
+            String intermediateFieldName = arbNode.getIntermediateValueFieldName();
+            if (intermediateFieldName != null)
+            {
+              fieldName = intermediateFieldName;
+            }
+          }
+
+          // Add edge with field name as label, with reversed direction
+          dot.append(String.format("  \"%s\" -> \"%s\" [label=\"%s\"];\n",
+                                   child.toString(), // child is now the source
+                                   current.toString(), // current is now the target
+                                   fieldName));
+        }
+      }
+    }
+
+    dot.append("}\n");
+
+    // Save to file
+    try ( PrintWriter out = new PrintWriter(filePath))
+    {
+      out.println(dot.toString());
+    }
+    catch (FileNotFoundException e)
+    {
+      throw new RuntimeException("Failed to write DOT file: " + e.getMessage(),
+                                 e);
+    }
+  }
+
   public ExpressionTree(Node<D, R, F> root)
   {
     indexBranches(this.root = root);
@@ -28,8 +98,8 @@ public class ExpressionTree<D, R, F extends Function<? extends D, ? extends R>> 
 
   public ArrayList<Node<D, R, F>>                    nodes           = new ArrayList<>();
 
-  public HashMap<Node<D, R, F>, List<Node<D, R, F>>> indexedBranches = new HashMap<Node<D, R, F>,
-                List<Node<D, R, F>>>();
+  public HashMap<Node<D, R, F>, List<Node<D, R, F>>> indexedBranches =
+                                                                     new HashMap<Node<D, R, F>, List<Node<D, R, F>>>();
 
   void indexBranches(Node<D, R, F> stem)
   {
