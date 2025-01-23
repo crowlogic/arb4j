a182c2b929d1ec3abedf186eed0c90190d75b174
Stephen Crowley <stephencrowley214@gmail.com>
2025-01-23 12:36:22 -0600

    axeiomatic

---
+++ src/main/java/arb/logic/Axiom.java
@@ -0,0 +1,17 @@
+package arb.logic;
+
+/**
+ * (Logic & Math.) A self-evident and necessary truth, or a proposition whose
+ * truth is so evident as first sight that no reasoning or demonstration can
+ * make it plainer; a proposition which it is necessary to take for granted; as,
+ * "The whole is greater than a part;" "A thing can not, at the same time, be
+ * and not be." [1913 Webster]
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ */
+public interface Axiom extends
+                       RegularProposition
+{
+
+}
