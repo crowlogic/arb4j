6a6f63c1492169069694d85baf70958309590976
Stephen Crowley <stephencrowley214@gmail.com>
2025-01-23 11:15:28 -0600

    2d00

--- src/main/java/arb/functions/polynomials/orthogonal/OrthogonalPolynomialExpansion.java
+++ src/main/java/arb/functions/polynomials/orthogonal/OrthogonalPolynomialExpansion.java
@@ -3,15 +3,22 @@ package arb.functions.polynomials.orthogonal;
 import java.util.ArrayList;
 
 import arb.Field;
+import arb.Polynomial;
+import arb.RationalFunction;
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
 import arb.functions.Function;
+import arb.functions.real.RealFunction;
 
 /**
+ * TODO: abstract this so it can be an orthogonal function sequence, like
+ * {@link RealFunction} or {@link RationalFunction} instead of being relagated
+ * to orthogonal {@link Polynomial}s
  * 
- * @param <D>
- * @param <C>
- * @param <F>
+ * @param <D> the domain of the function being expanded
+ * @param <C> the codomain of the function being expande
+ * @param <F> the type of the this{@link #basisElements} that comprise the
+ *            this{@link #basis}
  * @param <O> the type of {@link OrthogonalPolynomialSequence} that will be used
  *            to
  * 
@@ -22,7 +29,7 @@ public abstract class OrthogonalPolynomialExpansion<D extends Field<?>, V, C,
               O extends OrthogonalPolynomialSequence<? extends D, ? extends V, ? extends C>> implements
                                                    Function<D, V>
 {
-  public final D projections;
+  public final D            projections;
   public final ArrayList<C> basisElements;
 
   public OrthogonalPolynomialExpansion(O basis, D projections)
