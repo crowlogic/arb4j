98d7335d2c93e395dfa2bfc1685a772d1cf61aa2
steve <a@b.c>
2025-05-12 00:00:03 -0500

    weakly harmonizable processes are oscillatory, not the other way around

--- src/main/java/arb/documentation/concepts/OscillatoryProcess.java
+++ src/main/java/arb/documentation/concepts/OscillatoryProcess.java
@@ -2,13 +2,14 @@ package arb.documentation.concepts;
 
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
+import arb.documentation.concepts.stochastic.StochasticProcess;
 
 /**
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
  *      {@link TheArb4jLibrary}
  */
 public interface OscillatoryProcess extends
-                                    StronglyHarmonizableProcess
+                                    StochasticProcess
 {
 
 }
