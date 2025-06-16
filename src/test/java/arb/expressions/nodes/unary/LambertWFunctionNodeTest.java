028883dcfd03628afaf76a0cffd120f66dec3a5f
Steve <stephencrowley214@gmail.com>
2025-06-16 18:03:21 -0500

    ...

--- src/test/java/arb/expressions/nodes/unary/LambertWFunctionNodeTest.java
+++ src/test/java/arb/expressions/nodes/unary/LambertWFunctionNodeTest.java
@@ -1,6 +1,5 @@
 package arb.expressions.nodes.unary;
 
-import arb.expressions.Expression;
 import arb.functions.real.RealFunction;
 import junit.framework.TestCase;
 
@@ -9,7 +8,6 @@ public class LambertWFunctionNodeTest extends
 {
   public static void testIt()
   {
-    Expression.saveClasses = true;
     var expr = RealFunction.express("W(0,x)");
     assertEquals(0.9182235367998267, expr.eval(2.3));
   }
