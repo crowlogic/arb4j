6f9040aaa74ca5d920faeefae34fb7638e9ca156
Steve <stephencrowley214@gmail.com>
2025-06-18 06:05:59 -0500

    the fucking sun is coming up time to hit the sack

--- src/test/java/arb/stochastic/RealWhiteNoiseProcessTest.java
+++ src/test/java/arb/stochastic/RealWhiteNoiseProcessTest.java
@@ -1,10 +1,17 @@
 package arb.stochastic;
 
 import arb.Real;
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
 import junit.framework.TestCase;
 
-public class RandomStandardNormalStreamTest extends
-                                            TestCase
+/**
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ **/
+public class RealWhiteNoiseProcessTest extends
+                                       TestCase
 {
   public void testWhiteNoiseSampleStream()
   {
@@ -23,6 +30,7 @@ public class RandomStandardNormalStreamTest extends
                                .average()
                                .getAsDouble();
       assertEquals(mean, mean2);
+      whiteNoise.initializeWithSeed(777);
       double variance = whiteNoise.stream(128, limit)
                                   .mapToDouble(x -> x.pow(2, bits).doubleValue())
                                   .average()
