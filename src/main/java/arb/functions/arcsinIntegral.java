8768dec4c5f6b783066a80736f35a711915e1429
ᵏ₇₅¾½ᴮΠ₅ᴹμ₃Λᵏ1ₒ₍⅒βᴰ5ᵛ <a@b.c>
2025-06-22 13:45:55 -0500

    added decompiled stuff #685

---
+++ src/main/java/arb/functions/arcsinIntegral.java
@@ -0,0 +1,79 @@
+package arb.functions;
+
+import arb.Initializable;
+import arb.Real;
+import arb.Typesettable;
+import arb.functions.real.RealFunction;
+
+public class arcsinIntegral implements
+                            RealFunction,
+                            Typesettable,
+                            AutoCloseable,
+                            Initializable
+{
+  public boolean       isInitialized;
+  public Real          lowerValueℝ0001 = new Real();
+  public Real          upperValueℝ0001 = new Real();
+  public integralℝ0001 integralℝ0001;
+
+  @Override
+  public Class<Real> domainType()
+  {
+    return Real.class;
+  }
+
+  @Override
+  public Class<Real> coDomainType()
+  {
+    return Real.class;
+  }
+
+  @Override
+  public Real evaluate(Real in, int order, int bits, Real result)
+  {
+    if (!this.isInitialized)
+    {
+      this.initialize();
+    }
+
+    return in.arcsin(bits, super.vℝ0004);
+  }
+
+  @Override
+  public void initialize()
+  {
+    if (this.isInitialized)
+    {
+      throw new AssertionError("Already initialized");
+    }
+    else
+    {
+      if (this.integralℝ0001 == null)
+      {
+        this.integralℝ0001 = new integralℝ0001();
+      }
+
+      this.isInitialized = true;
+    }
+  }
+
+  @Override
+  public void close()
+  {
+    this.lowerValueℝ0001.close();
+    this.upperValueℝ0001.close();
+    this.integralℝ0001.close();
+  }
+
+  @Override
+  public String toString()
+  {
+    return "∫y➔1/√(1-y^2)dy∈(-1,x)";
+  }
+
+  @Override
+  public String typeset()
+  {
+    return "\\int \\frac{1}{{(\\left(1-{y}^{2}\\right))}^{\\frac{1}{2}}} \\,dy";
+  }
+}
