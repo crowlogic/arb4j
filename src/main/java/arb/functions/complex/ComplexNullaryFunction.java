1e586d2ee8d585456d5b0452ffe752f2502fe9ed
Stephen Crowley <stephencrowley214@gmail.com>
2025-07-23 00:58:34 -0500

    only print function name if its not the class name which is just literally the expression also stuff for #709

--- src/main/java/arb/functions/complex/ComplexNullaryFunction.java
+++ src/main/java/arb/functions/complex/ComplexNullaryFunction.java
@@ -4,6 +4,8 @@ import arb.Complex;
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
 import arb.expressions.Context;
+import arb.expressions.Expression;
+import arb.expressions.Parser;
 import arb.functions.Function;
 import arb.functions.NullaryFunction;
 
@@ -15,7 +17,28 @@ import arb.functions.NullaryFunction;
 public interface ComplexNullaryFunction extends
                                         NullaryFunction<Complex>
 {
-  public static ComplexNullaryFunction express(String functionName, String expression, Context context)
+
+  public static Expression<Object, Complex, ComplexNullaryFunction> parse(String expression)
+  {
+    return NullaryFunction.parse(Complex.class,
+                                 ComplexNullaryFunction.class,
+                                 Parser.expressionToUniqueClassname(expression),
+                                 expression,
+                                 null);
+  }
+
+  public static Expression<Object, Complex, ComplexNullaryFunction> parse(String expression,
+                                                                          Context context)
+  {
+    return NullaryFunction.parse(Complex.class,
+                                 ComplexNullaryFunction.class,
+                                 Parser.expressionToUniqueClassname(expression),
+                                 expression,
+                                 context);
+  }
+
+  public static ComplexNullaryFunction
+         express(String functionName, String expression, Context context)
   {
     return Function.instantiate(expression,
                                 context,
