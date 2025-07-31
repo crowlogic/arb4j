aa34168b6c030f4c3bf4b861c63eb087bae97a30
Stephen Crowley <stephencrowley214@gmail.com>
2025-07-31 00:42:34 -0500

    context var is not being injected into the inner function of the outer function java.lang.NoSuchMethodError: 'arb.Real arb.Real.θ(int, arb.Real)' 	at Afunc.evaluate(Unknown Source) 	at arb4j/arb.functions.real.RealFunction.lambda$0(RealFunction.java:430) 	at java.base/java.util.stream.Streams$RangeIntSpliterator.forEachRemaining(Streams.java:104) 	at java.base/java.util.stream.IntPipeline$Head.forEach(IntPipeline.java:620) 	at arb4j/arb.functions.real.RealFunction.quantize(RealFunction.java:430) 	at arb4j/arb.functions.real.RealFunction.quantize(RealFunction.java:374) 	at arb4j/arb.functions.real.RealFunction.quantize(RealFunction.java:336) 	at arb4j/arb.utensils.ShellFunctions.lambda$6(ShellFunctions.java:207) 	at javafx.graphics@24.0.1/com.sun.javafx.application.PlatformImpl.lambda$runLater$4(PlatformImpl.java:419) 	at javafx.graphics@24.0.1/com.sun.glass.ui.InvokeLaterDispatcher$Future.run(InvokeLaterDispatcher.java:95) 	at javafx.graphics@24.0.1/com.sun.glass.ui.gtk.GtkApplication._runLoop(Native Method) 	at javafx.graphics@24.0.1/com.sun.glass.ui.gtk.GtkApplication.lambda$runLoop$1(GtkApplication.java:240) 	at java.base/java.lang.Thread.run(Thread.java:1447)

--- src/main/java/arb/stochastic/processes/RealZProcess.java
+++ src/main/java/arb/stochastic/processes/RealZProcess.java
@@ -1,22 +1,32 @@
 package arb.stochastic.processes;
 
+import arb.Real;
 import arb.expressions.Context;
 import arb.functions.RealBivariateFunction;
 import arb.functions.real.RealFunction;
+import arb.utensils.ShellFunctions;
 
 public class RealZProcess implements
                           RealOscillatoryProcess
 {
+  public static void main(String args[])
+  {
+    RealZProcess Zprocess = new RealZProcess();
+    RealFunction gain     = Zprocess.A.evaluate(Real.named("λ").one(), 128);
+    ShellFunctions.plot(0, 200, 2000, gain);
+
+  }
+
   final Context               context = new Context();
 
   final RealFunction          ϑ       = RealFunction.express("ϑ:t->im(lnΓ(1/4+I*t/2))-(log(π)/2)*t",
                                                              context);
 
   final RealBivariateFunction A       =
-                                RealBivariateFunction.express("A:t->exp(I*λ*(θ(t)-t))", context);
+                                RealBivariateFunction.express("A:exp(I*λ*(θ(t)-t))", context);
 
   final RealBivariateFunction ϕ       =
-                                RealBivariateFunction.express("ϕ:t->exp(I*λ*θ(t))", context);
+                                RealBivariateFunction.express("ϕ:exp(I*λ*θ(t))", context);
 
   @Override
   public RealBivariateFunction gainFuction()
