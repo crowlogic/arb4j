c52349d47cbd8feffdec722e1276d98437ae8ca2
Steve <stephencrowley214@gmail.com>
2025-08-01 22:23:09 -0500

    ...

--- src/test/java/arb/functions/integer/RealFunctionSequenceTest.java
+++ src/test/java/arb/functions/integer/RealFunctionSequenceTest.java
@@ -3,7 +3,6 @@ package arb.functions.integer;
 import arb.Real;
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
-import arb.expressions.Expression;
 import junit.framework.TestCase;
 
 /**
@@ -16,8 +15,8 @@ public class RealFunctionSequenceTest extends
 {
   public static void testRealFunctionSequence()
   {
-    Expression.trace = Expression.saveClasses = true;
-    
+    // Expression.trace = Expression.saveClasses = true;
+
     var    f  =
              RealFunctionSequence.express("f:k➔√((2*k+½)/π)*((k+1)⋰-½)^2*√((8*k+2)/π)*(-1)^k*j(2*k,x)");
     var    f3 = f.evaluate(3, 128);
