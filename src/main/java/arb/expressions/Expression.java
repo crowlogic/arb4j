5f3b62c67ffaeb267417e7c031de556eadd50246
Steve <stephencrowley214@gmail.com>
2025-08-13 09:43:08 -0500

    refactor

--- src/main/java/arb/expressions/Expression.java
+++ src/main/java/arb/expressions/Expression.java
@@ -1,51 +1,15 @@
 package arb.expressions;
 
-import static arb.expressions.Compiler.addNullCheckForField;
-import static arb.expressions.Compiler.cast;
-import static arb.expressions.Compiler.defineMethod;
-import static arb.expressions.Compiler.designateLabel;
-import static arb.expressions.Compiler.duplicateTopOfTheStack;
-import static arb.expressions.Compiler.generateFunctionInterface;
-import static arb.expressions.Compiler.generateNewObjectInstruction;
-import static arb.expressions.Compiler.generateReturnFromVoidMethod;
-import static arb.expressions.Compiler.getField;
-import static arb.expressions.Compiler.getFieldFromThis;
-import static arb.expressions.Compiler.getVariablePrefix;
-import static arb.expressions.Compiler.invokeCloseMethod;
-import static arb.expressions.Compiler.invokeDefaultConstructor;
-import static arb.expressions.Compiler.invokeSetMethod;
-import static arb.expressions.Compiler.jumpToIfNotEqual;
-import static arb.expressions.Compiler.loadFunctionClass;
-import static arb.expressions.Compiler.loadResultParameter;
-import static arb.expressions.Compiler.loadThisOntoStack;
-import static arb.expressions.Compiler.swap;
-import static arb.expressions.Parser.isAlphabeticalGreekSpecialOrBlackLetter;
-import static arb.expressions.Parser.isNumeric;
-import static arb.expressions.Parser.isSubscript;
-import static arb.expressions.Parser.isSuperscript;
-import static arb.expressions.Parser.transformToJavaAcceptableCharacters;
+import static arb.expressions.Compiler.*;
+import static arb.expressions.Parser.*;
 import static java.lang.String.format;
 import static java.lang.System.err;
-import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
-import static org.objectweb.asm.Opcodes.ATHROW;
-import static org.objectweb.asm.Opcodes.GETFIELD;
-import static org.objectweb.asm.Opcodes.IFEQ;
-import static org.objectweb.asm.Opcodes.INVOKESPECIAL;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
-import static org.objectweb.asm.Opcodes.NEW;
-import static org.objectweb.asm.Opcodes.PUTFIELD;
+import static org.objectweb.asm.Opcodes.*;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
@@ -53,67 +17,22 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.Label;
-import org.objectweb.asm.MethodVisitor;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.Type;
+import org.objectweb.asm.*;
 import org.objectweb.asm.util.TraceClassVisitor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import arb.Becomable;
-import arb.Complex;
-import arb.ComplexFraction;
-import arb.ComplexPolynomial;
-import arb.ComplexRationalFunction;
-import arb.Fraction;
-import arb.GaussianInteger;
-import arb.Initializable;
+import arb.*;
 import arb.Integer;
-import arb.IntegerPolynomial;
-import arb.Named;
-import arb.OrderedPair;
-import arb.RationalFunction;
-import arb.Real;
-import arb.RealPolynomial;
-import arb.Typesettable;
 import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
 import arb.documentation.TheArb4jLibrary;
 import arb.exceptions.CompilerException;
 import arb.expressions.context.Dependency;
 import arb.expressions.context.TopologicalSorter;
-import arb.expressions.nodes.DerivativeNode;
-import arb.expressions.nodes.ElseNode;
-import arb.expressions.nodes.FractionalDerivativeNode;
-import arb.expressions.nodes.IntegralNode;
-import arb.expressions.nodes.LiteralConstantNode;
-import arb.expressions.nodes.Node;
-import arb.expressions.nodes.VariableNode;
-import arb.expressions.nodes.VectorNode;
-import arb.expressions.nodes.binary.AdditionNode;
-import arb.expressions.nodes.binary.AscendingFactorializationNode;
-import arb.expressions.nodes.binary.BinaryOperationNode;
-import arb.expressions.nodes.binary.DivisionNode;
-import arb.expressions.nodes.binary.MultiplicationNode;
-import arb.expressions.nodes.binary.SubtractionNode;
-import arb.expressions.nodes.nary.NAryOperationNode;
-import arb.expressions.nodes.nary.ProductNode;
-import arb.expressions.nodes.nary.SumNode;
-import arb.expressions.nodes.unary.BesselFunctionNodeOfTheFirstKind;
-import arb.expressions.nodes.unary.BetaFunctionNode;
-import arb.expressions.nodes.unary.BinomialCoefficientNode;
-import arb.expressions.nodes.unary.FactorialNode;
-import arb.expressions.nodes.unary.FloorNode;
-import arb.expressions.nodes.unary.FunctionNode;
-import arb.expressions.nodes.unary.FunctionalEvaluationNode;
-import arb.expressions.nodes.unary.HypergeometricFunctionNode;
-import arb.expressions.nodes.unary.LambertWFunctionNode;
-import arb.expressions.nodes.unary.LommelPolynomialNode;
-import arb.expressions.nodes.unary.SphericalBesselFunctionNodeOfTheFirstKind;
-import arb.expressions.nodes.unary.UnaryOperationNode;
-import arb.expressions.nodes.unary.WhenNode;
+import arb.expressions.nodes.*;
+import arb.expressions.nodes.binary.*;
+import arb.expressions.nodes.nary.*;
+import arb.expressions.nodes.unary.*;
 import arb.expressions.viz.ExpressionTree;
 import arb.functions.Function;
 import arb.functions.RealToComplexFunction;
@@ -1285,20 +1204,23 @@ public class Expression<D, C, F extends Function<? extends D, ? extends C>> impl
   {
     context.functions.map.entrySet()
                          .stream()
-                         .filter(entry -> function.referencedFunctions.containsKey(entry.getKey()))
-                         .forEach(entry ->
-                         {
-                           var fieldName       = entry.getKey();
-                           FunctionMapping<?, ?, ?> functionMapping = entry.getValue();
-                           var fieldType       = functionMapping.functionClass;
-                           if (!fieldName.equals(functionName))
-                           {
-                             loadThisFieldOntoStack(duplicateTopOfTheStack(mv),
-                                                    fieldName,
-                                                    fieldType);
-                             Compiler.putField(mv, function.className, fieldName, fieldType);
-                           }
-                         });
+                         .filter(entry -> function.referencedFunctions.containsKey(entry.getKey())
+                                       && !functionName.equals(entry.getKey()))
+                         .forEach(entry -> propagateContextFunctionToFunctionalElement(mv,
+                                                                                       function,
+                                                                                       entry.getKey(),
+                                                                                       entry.getValue().functionClass));
+  }
+
+  protected void propagateContextFunctionToFunctionalElement(MethodVisitor mv,
+                                                             Expression<Object,
+                                                                           Object,
+                                                                           Function<?, ?>> function,
+                                                             String fieldName,
+                                                             Class<?> fieldType)
+  {
+    loadThisFieldOntoStack(duplicateTopOfTheStack(mv), fieldName, fieldType);
+    Compiler.putField(mv, function.className, fieldName, fieldType);
   }
 
   public void propagateContexVariablesToFunctionalElement(MethodVisitor mv,
