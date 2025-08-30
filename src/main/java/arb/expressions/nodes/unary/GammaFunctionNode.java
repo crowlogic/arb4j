221029ea0c6f3d8c38e21a0e85091416c54e4a0c
Stephen Crowley <stephencrowley214@gmail.com>
2025-08-29 17:46:06 -1000

    derivaitves #738

---
+++ src/main/java/arb/expressions/nodes/unary/GammaFunctionNode.java
@@ -0,0 +1,94 @@
+package arb.expressions.nodes.unary;
+
+import static arb.expressions.Compiler.invokeStaticMethod;
+import static arb.expressions.Compiler.loadBitsParameterOntoStack;
+
+import org.objectweb.asm.MethodVisitor;
+
+import arb.Complex;
+import arb.Real;
+import arb.arblib;
+import arb.expressions.Expression;
+import arb.expressions.nodes.Node;
+import arb.functions.Function;
+
+/**
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ **/
+public class GammaFunctionNode<D, C, F extends Function<? extends D, ? extends C>> extends
+                              PolySeriesFunctionNode<D, C, F>
+{
+
+  public GammaFunctionNode(Expression<D, C, F> expression)
+  {
+    super("Γ",
+          expression);
+  }
+
+  private GammaFunctionNode(Expression<D, C, F> expression, Node<D, C, F> arg, int order)
+  {
+    super("Γ",
+          expression,
+          arg,
+          order);
+  }
+
+  @Override
+  protected PolySeriesFunctionNode<D, C, F> makeWithOrder(int order)
+  {
+    return new GammaFunctionNode<>(expression,
+                                   arg,
+                                   order);
+  }
+
+  @Override
+  protected void pushSeriesCallParamsAndInvoke(MethodVisitor mv,
+                                               Class<?> sType,
+                                               boolean isComplex,
+                                               int n,
+                                               int oneSlot)
+  {
+    // n
+    pushInt(mv, n);
+    // prec
+    loadBitsParameterOntoStack(mv);
+
+    if (isComplex)
+    {
+      // (resPoly, fPoly, n, prec)
+      invokeStaticMethod(mv,
+                         arblib.class,
+                         "acb_poly_gamma_series",
+                         Void.class,
+                         Complex.class,
+                         Complex.class,
+                         int.class,
+                         int.class);
+    }
+    else
+    {
+      invokeStaticMethod(mv,
+                         arblib.class,
+                         "arb_poly_gamma_series",
+                         Void.class,
+                         Real.class,
+                         Real.class,
+                         int.class,
+                         int.class);
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format("Γ(%s)", arg);
+  }
+
+  @Override
+  public String typeset()
+  {
+    return String.format("\\\\Gamma(%s)", arg == null ? "" : arg.typeset());
+  }
+}
---
+++ src/main/java/arb/expressions/nodes/unary/LogGammaFunctionNode.java
@@ -0,0 +1,96 @@
+package arb.expressions.nodes.unary;
+
+import static arb.expressions.Compiler.invokeStaticMethod;
+import static arb.expressions.Compiler.loadBitsParameterOntoStack;
+
+import org.objectweb.asm.MethodVisitor;
+
+import arb.Complex;
+import arb.Real;
+import arb.arblib;
+import arb.documentation.BusinessSourceLicenseVersionOnePointOne;
+import arb.documentation.TheArb4jLibrary;
+import arb.expressions.Expression;
+import arb.expressions.nodes.Node;
+import arb.functions.Function;
+
+/**
+ * 
+ * @see BusinessSourceLicenseVersionOnePointOne © terms of the
+ *      {@link TheArb4jLibrary}
+ **/
+public class LogGammaFunctionNode<D, C, F extends Function<? extends D, ? extends C>> extends
+                                 PolySeriesFunctionNode<D, C, F>
+{
+
+  public LogGammaFunctionNode(Expression<D, C, F> expression)
+  {
+    super("lnΓ",
+          expression);
+  }
+
+  private LogGammaFunctionNode(Expression<D, C, F> expression, Node<D, C, F> arg, int order)
+  {
+    super("lnΓ",
+          expression,
+          arg,
+          order);
+  }
+
+  @Override
+  protected PolySeriesFunctionNode<D, C, F> makeWithOrder(int order)
+  {
+    return new LogGammaFunctionNode<>(expression,
+                                      arg,
+                                      order);
+  }
+
+  @Override
+  protected void pushSeriesCallParamsAndInvoke(MethodVisitor mv,
+                                               Class<?> sType,
+                                               boolean isComplex,
+                                               int n,
+                                               int oneSlot)
+  {
+    // n
+    pushInt(mv, n);
+    // prec
+    loadBitsParameterOntoStack(mv);
+
+    if (isComplex)
+    {
+      // (resPoly, fPoly, n, prec)
+      invokeStaticMethod(mv,
+                         arblib.class,
+                         "acb_poly_lgamma_series",
+                         Void.class,
+                         Complex.class,
+                         Complex.class,
+                         int.class,
+                         int.class);
+    }
+    else
+    {
+      invokeStaticMethod(mv,
+                         arblib.class,
+                         "arb_poly_lgamma_series",
+                         Void.class,
+                         Real.class,
+                         Real.class,
+                         int.class,
+                         int.class);
+    }
+  }
+
+  @Override
+  public String toString()
+  {
+    return String.format("lnΓ(%s)", arg);
+  }
+
+  @Override
+  public String typeset()
+  {
+    return String.format("\\\\ln\\\\Gamma(%s)", arg == null ? "" : arg.typeset());
+  }
+}
