976c4eaaf1d33c64a7dbb896313870947cb854a0
Stephen Crowley <stephencrowley214@gmail.com>
2025-09-05 20:07:53 -1000

    ...#746

--- src/test/java/arb/expressions/nodes/binary/ExponentiationNodeTest.java
+++ src/test/java/arb/expressions/nodes/binary/ExponentiationNodeTest.java
@@ -1,15 +1,34 @@
 package arb.expressions.nodes.binary;
 
+import arb.Real;
+import arb.RealConstants;
+import arb.expressions.Context;
+import arb.expressions.Expression;
+import arb.functions.RealBivariateToComplexFunction;
+import arb.functions.complex.RealRiemannSiegelThetaFunction;
 import arb.functions.real.RealFunction;
 import junit.framework.TestCase;
 
 public class ExponentiationNodeTest extends
                                     TestCase
 {
+  public static void testDerivativesAndExponentials()
+  {
+    Expression.saveClasses = Expression.trace = true;
+    var context = new Context();
+    var θ       = new RealRiemannSiegelThetaFunction();
+//    final RealFunction                   θ        =
+//                  RealFunction.express("θ:im(lnΓ(¼+ⅈ*t/2))-(log(π)/2)*t",
+//                                       context);
+    context.registerFunction("θ", θ);
+    var gain = RealBivariateToComplexFunction.express("A:exp(ⅈ*λ*(θ(t)-t))*√(θ̇(t))", context);
+    gain.evaluate(RealConstants.zero, 128);
+  }
+
   public static void testSimplifySquaredSquareRoots()
   {
     var f = RealFunction.parse("x^(1/2)*x^(1/2)");
     f.simplify();
-    assertEquals( "x➔x", f.toString() );
+    assertEquals("x➔x", f.toString());
   }
 }
---
+++ src/test/java/arb/stochastic/processes/ZProcessTest.java
@@ -0,0 +1,16 @@
+package arb.stochastic.processes;
+
+import arb.expressions.Expression;
+import junit.framework.TestCase;
+
+public class ZProcessTest extends
+                          TestCase
+{
+
+  public void testOscillatoryProcessGainFunction()
+  {
+    Expression.trace = Expression.saveClasses = true;
+    ZProcess Zprocess = new ZProcess();
+  }
+
+}
