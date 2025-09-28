a3acc1636d6e912dc189131c45e482939bea3dfe
Stephen Crowley <stephencrowley214@gmail.com>
2025-09-28 04:02:14 -0500

    luv #748

--- src/test/java/arb/expressions/nodes/binary/ExponentiationNodeTest.java
+++ src/test/java/arb/expressions/nodes/binary/ExponentiationNodeTest.java
@@ -5,6 +5,7 @@ import arb.expressions.Context;
 import arb.expressions.Expression;
 import arb.functions.RealBivariateToComplexFunction;
 import arb.functions.real.RealFunction;
+import arb.functions.real.RealRiemannSiegelThetaFunction;
 import junit.framework.TestCase;
 
 public class ExponentiationNodeTest extends
@@ -36,10 +37,9 @@ public class ExponentiationNodeTest extends
     {
       Expression.saveClasses = Expression.trace = true;
       var                context = new Context();
-//      var θ       = new RealRiemannSiegelThetaFunction();
-//      context.registerFunction("θ", θ);
+      var θ       = new RealRiemannSiegelThetaFunction();
+      context.registerFunction("θ", θ);
       
-      final RealFunction θ       = RealFunction.express("θ:im(lnΓ(¼+ⅈ*t/2))-(log(π)/2)*t", context);
       var gain = RealBivariateToComplexFunction.express("A:exp(ⅈ*λ*(θ(t)-t))*√(θ̇(t))", context);
       gain.evaluate(RealConstants.zero, 128);
     }
