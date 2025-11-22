2bf65e4c148a75869c6a07ab033680b6a9d7f2f4
Stephen  Crowley <stephencrowley214@gmail.com>
2025-11-22 08:14:09 -0600

    ...

--- src/main/java/arb/measure/Measure.java
+++ src/main/java/arb/measure/Measure.java
@@ -6,14 +6,17 @@ import arb.documentation.TheArb4jLibrary;
 import arb.functions.Function;
 
 /**
- * A {@link Measure} is a {@link Function} from a {@link σField} over X denoted
- * Σ to the {@link Real} numbers if it satisfies the following properties:
+ * A {@link Function} from a {@link σField} over X denoted
+ * Σ to the {@link Real} numbers is a {@link Measure} if it is:
  * 
  * <pre>
- * Non-negativity: μ(E)≥0 ∀ E∈Σ 
- * Null empty set: μ(∅)=0 
- * σ-additivity  : μ(∑Eₖ(k=1..∞))=∑μ(Eₖ)(k=1..∞) ∀ {Eₖ}⊂Σ
- * </pre>
+ * 1. non-negative for all Borel sets E∈Σ                         
+ *       μ(E)≥0   
+ * 2. equals zero on the empty set:                                 
+ *       μ(∅)=0 
+ * 3. σ-additive (the measure of the sum is the sum of the measure)                        
+ *       μ(∑Eₖ)=∑μ(Eₖ)∀{Eₖ}⊂Σ
+ *  </pre>
 
  * @see BusinessSourceLicenseVersionOnePointOne © terms of the
  *      {@link TheArb4jLibrary}
