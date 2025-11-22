28b8cb68915b61e7c50d5b7d47f77965c9b3fd3a
Stephen  Crowley <stephencrowley214@gmail.com>
2025-11-22 08:25:16 -0600

    arb.measure.MeasureSpace<X, Σ extends σField<? extends X>, μ extends Measure<? extends X, ? extends Σ>>
    A MeasureSpace is a basic Space of MeasureTheory which generalizes the
    notion of volume. It contains an underlying set X, the measurable
    subsets constituting the σField and the method that is used for
    measuring (the Measure). One important example of a measure space is a
    ProbabilitySpace. A measurable space consists of the first two
    components without a specific measure, thus. A measure space is a triple
    (X,Σ,μ) where
    
     X is a set
     Σ is the σField of the set X
     μ()  is a Measure on the MeasurableSpace (X,Σ)
    
    In other words, a measure space consists of a MeasurableSpace (X,Σ )
    together with a Measure on it and thus MeasureSpace extends
    MeasurableSpace and provides the method μ() to specify the measure

--- src/main/java/arb/measure/MeasureSpace.java
+++ src/main/java/arb/measure/MeasureSpace.java
@@ -8,21 +8,21 @@ import arb.space.topological.Space;
  * A {@link MeasureSpace} is a basic {@link Space} of {@link MeasureTheory}
  * which generalizes the notion of volume. It contains an underlying set X, the
  * measurable subsets constituting the {@link σField} and the method that is
- * used for measuring (the {@link Measure}). One important example of a measure
- * space is a {@link ProbabilitySpace}.
+ * used for measuring (the {@link Measure}). One important example of a
+ * {@link MeasureSpace} is a {@link ProbabilitySpace}.
  * 
  * A measurable space consists of the first two components without a specific
  * measure, thus.
  * 
- * A measure space is a triple <b>(Σ,A,μ)</b> where<br>
+ * A measure space is a triple <b>(X,Σ,μ)</b> where<br>
  * 
  * <pre>
  * X is a set
  * Σ is the {@link σField} of the set X
- * μ is a {@link Measure} on the {@link MeasurableSpace} (X,Σ)
+ * {@link #μ()}  is a {@link Measure} on the {@link MeasurableSpace} (X,Σ)
  * </pre>
  * 
- * In other words, a measure space consists of a {@link MeasurableSpace} (Σ,A )
+ * In other words, a measure space consists of a {@link MeasurableSpace} (X,Σ )
  * together with a {@link Measure} on it and thus {@link MeasureSpace} extends
  * {@link MeasurableSpace} and provides the method {@link #μ()} to specify the
  * measure
