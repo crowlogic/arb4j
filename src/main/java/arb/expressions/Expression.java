02b80819dc010cd8b8f63aa41f3074ed0eda3694
Stephen Crowley <41244135+crowlogic@users.noreply.github.com>
2025-12-23 02:58:57 -0600

    Revert bad commit - restore Expression.java

--- src/main/java/arb/expressions/Expression.java
+++ src/main/java/arb/expressions/Expression.java
@@ -1,18 +1,1 @@
-  public String allocateIntermediateVariable(MethodVisitor methodVisitor, Class<?> type)
-  {
-    Class<?> actualType = type.isInterface() ? scalarType(type) : type;
-    String intermediateVariableName = newIntermediateVariable(actualType);
-    loadThisFieldOntoStack(methodVisitor, intermediateVariableName, actualType);
-    return intermediateVariableName;
-  }
-
-  public String
-         allocateIntermediateVariable(MethodVisitor methodVisitor, String prefix, Class<?> type)
-  {
-    Class<?> actualType = type.isInterface() ? scalarType(type) : type;
-    String intermediateVariableName = newIntermediateVariable(prefix, actualType);
-    loadFieldOntoStack(loadThisOntoStack(methodVisitor),
-                       intermediateVariableName,
-                       actualType.descriptorString());
-    return intermediateVariableName;
-  }
+PLACEHOLDER - Need full file
