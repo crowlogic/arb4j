8a01c11c5a28a46965b1f045b8e8a560c972051a
crow <crow@core>
2026-01-28 06:59:06 -0600

    ... #813

--- src/main/java/arb/expressions/ExpressionClassLoader.java
+++ src/main/java/arb/expressions/ExpressionClassLoader.java
@@ -32,14 +32,14 @@ public class ExpressionClassLoader extends
   @Override
   protected Class<?> findClass(String name) throws ClassNotFoundException
   {
-    if (Expression.trace)
-    {
-      log.debug("\n\nfindClass('{}') in classes {{}} of Context#{} which has functions={}\n",
-                name,
-                compiledClasses.keySet(),
-                System.identityHashCode(context),
-                context.functions);
-    }
+//    if (Expression.trace)
+//    {
+//      log.debug("\n\nfindClass('{}') in classes {{}} of Context#{} which has functions={}\n",
+//                name,
+//                compiledClasses.keySet(),
+//                System.identityHashCode(context),
+//                context.functions);
+//    }
 
     // First check compiledClasses map for direct class name match
 //    Class<?> directMatch = compiledClasses.get(name);
