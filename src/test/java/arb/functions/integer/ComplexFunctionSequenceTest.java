26d64b750c306a710dbee28b146977af2515317e
Stephen  Crowley <stephencrowley214@gmail.com>
2026-02-01 21:01:59 -1000

    ...

--- src/test/java/arb/functions/integer/ComplexFunctionSequenceTest.java
+++ src/test/java/arb/functions/integer/ComplexFunctionSequenceTest.java
@@ -7,14 +7,14 @@ import junit.framework.TestCase;
 public class ComplexFunctionSequenceTest extends
                                          TestCase
 {
-  public static void testSeq1()
+  public static void testComplexFunctionSeq1()
   {
 
     ComplexFunctionSequence seq      =
                                 ComplexFunctionSequence.express("χ", "i➔p➔int(t➔t^(i+p), t=-1..1)");
     ComplexFunction         function = seq.apply(3);
     Complex                 y        = function.eval(2.3, new Complex());
-    assertEquals("0.2995821230740516 +/- 6.69e-17 + i*0.1526447159198013 +/- 5.15e-17",
+    assertEquals("blah +/- 5.15e-17",
                  y.toString());
   }
 }
