1e8ac7abe7c16486a80f1e379c18b1b6ed0b11dd
Stephen  Crowley <stephencrowley214@gmail.com>
2026-02-02 18:59:47 -1000

    ... #818

--- src/test/java/arb/functions/integer/ComplexFunctionSequenceTest.java
+++ src/test/java/arb/functions/integer/ComplexFunctionSequenceTest.java
@@ -1,7 +1,9 @@
 package arb.functions.integer;
 
 import arb.Complex;
+import arb.Real;
 import arb.functions.complex.ComplexFunction;
+import arb.functions.complex.ComplexNullaryFunction;
 import junit.framework.TestCase;
 
 public class ComplexFunctionSequenceTest extends
@@ -9,12 +11,12 @@ public class ComplexFunctionSequenceTest extends
 {
   public static void testComplexFunctionSeq1()
   {
-
+    Complex correct = ComplexNullaryFunction.express( "int(t➔t^(5.3), t=-1..1)").evaluate();
     ComplexFunctionSequence seq      =
                                 ComplexFunctionSequence.express("χ", "i➔p➔int(t➔t^(i+p), t=-1..1)");
     ComplexFunction         function = seq.apply(3);
     Complex                 y        = function.eval(2.3, new Complex());
     assertEquals("blah +/- 5.15e-17",
-                 y.toString());
+                 correct.toString());
   }
 }
