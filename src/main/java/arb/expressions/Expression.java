02f5343a2eb37af671827eb21573c503474a8142
Stephen Crowley <41244135+crowlogic@users.noreply.github.com>
2026-02-25 16:51:42 -1000

    WIP: Expression.java - need to push full content

--- src/main/java/arb/expressions/Expression.java
+++ src/main/java/arb/expressions/Expression.java
@@ -1,4 +1,46 @@
 package arb.expressions;
 
-// [Expression.java - 4207 lines - contains newFractionLiteralConstant method at line 3171]
-// This is a placeholder - actual file needs to be pushed via git
+import static arb.expressions.Compiler.*;
+import static arb.expressions.Parser.*;
+import static java.lang.String.format;
+import static org.objectweb.asm.Opcodes.*;
+
+import java.io.File;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.*;
+import java.util.stream.*;
+
+import org.objectweb.asm.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import arb.*;
+import arb.Integer;
+import arb.exceptions.CompilerException;
+import arb.expressions.context.Dependency;
+import arb.expressions.nodes.*;
+import arb.expressions.nodes.Node;
+import arb.expressions.nodes.binary.*;
+import arb.expressions.nodes.nary.*;
+import arb.expressions.nodes.unary.*;
+import arb.functions.*;
+import arb.functions.Function;
+import arb.functions.complex.ComplexFunction;
+import arb.functions.integer.*;
+import arb.functions.real.RealFunction;
+import arb.utensils.Utensils;
+import arb.utensils.text.trees.TextTree;
+import arb.utensils.text.trees.TreeModel;
+
+// [NOTE: This is Expression.java - 4207 lines - the newFractionLiteralConstant method at line 3171 needs to be included]
+// Full file content is 154616 chars and needs to be pushed
+// The actual change adds:
+//   public LiteralConstantNode<D, C, F> newFractionLiteralConstant(Fraction frac)
+//   {
+//     return new LiteralConstantNode<>(this, frac);
+//   }
+// after the existing newLiteralConstant method (line ~3165)
+// 
+// INCOMPLETE PUSH - full file content needed
