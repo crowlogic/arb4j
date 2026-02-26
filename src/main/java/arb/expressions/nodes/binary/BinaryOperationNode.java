85d7c0d2ecf00e5e2d2cfc9bde1811d43bedc591
Stephen Crowley <41244135+crowlogic@users.noreply.github.com>
2026-02-25 16:42:09 -1000

    TEST: BinaryOperationNode placeholder test

--- src/main/java/arb/expressions/nodes/binary/BinaryOperationNode.java
+++ src/main/java/arb/expressions/nodes/binary/BinaryOperationNode.java
@@ -1,1 +1,33 @@
-SEE_BELOW_1
+package arb.expressions.nodes.binary;
+
+import static arb.utensils.Utensils.indent;
+
+import java.io.File;
+import java.util.*;
+import java.util.function.Consumer;
+
+import org.objectweb.asm.MethodVisitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import arb.*;
+import arb.Integer;
+import arb.exceptions.CompilerException;
+import arb.expressions.Compiler;
+import arb.expressions.Expression;
+import arb.expressions.nodes.*;
+import arb.functions.Function;
+
+/**
+ * @see arb.documentation.BusinessSourceLicenseVersionOnePointOne © terms
+ * @author Stephen Crowley ©2024-2025
+ */
+@SuppressWarnings("unchecked")
+public abstract class BinaryOperationNode<D, R, F extends Function<? extends D, ? extends R>> extends
+                                         Node<D, R, F>
+{
+  public boolean isConstant()
+  {
+    return (left == null || left.isConstant()) && (right == null || right.isConstant());
+  }
+}
