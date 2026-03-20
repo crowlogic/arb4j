be792f240400fe5b721c7659927d14a6ee14ef5e
Stephen  Crowley <stephencrowley214@gmail.com>
2026-03-19 20:13:45 -0500

    912: testUndefinedReferenceBecauseFunctionsTreatedLikeVariables: Failure to catch undefined reference.
    Task-Url: https://github.com/crowlogic/arb4j/issues/912

--- src/test/java/arb/expressions/nodes/VariableNodeTest.java
+++ src/test/java/arb/expressions/nodes/VariableNodeTest.java
@@ -1,8 +1,11 @@
 package arb.expressions.nodes;
 
 import arb.Real;
+import arb.RealPolynomial;
 import arb.expressions.Expression;
+import arb.functions.polynomials.RealPolynomialNullaryFunction;
 import arb.functions.real.RealFunction;
+import arb.utensils.ShellFunctions;
 import junit.framework.TestCase;
 
 public class VariableNodeTest extends
@@ -15,5 +18,14 @@ public class VariableNodeTest extends
     realExpr.substitute("x", x.sin());
     realExpr.substitute("x", x.div(2));
     assertEquals("x➔sin(x/2)^2", realExpr.toString());
+  }
+  
+  public void testResolutionOfNullaryPolynomialFunction()
+  {
+    Expression<Object, RealPolynomial, RealPolynomialNullaryFunction> expression = RealPolynomialNullaryFunction.parse("3*x+x^2");
+    RealPolynomialNullaryFunction f = expression.instantiate();
+    RealPolynomial poly = f.evaluate();
+    System.out.println( expression.inspect(f));
+    assertEquals( "x² + 3*x",poly.toString() );
   }
 }
