94f01485c0fcaac07ef9a0dc0bd3bd496585323c
Stephen Crowley <stephencrowley214@gmail.com>
2026-04-07 15:22:31 -0500

    922: Spectral representation
    Task-Url: https://github.com/crowlogic/arb4j/issues/922

---
+++ src/main/java/arb/operators/AbsoluteUnitaryTimeChange.java
@@ -0,0 +1,41 @@
+package arb.operators;
+
+import arb.Real;
+import arb.expressions.Context;
+import arb.expressions.Expression;
+import arb.functions.real.RealFunction;
+
+/**
+ * TODO: make an Operator which would be a Function<RealFunction,RealFunction>
+ * which would map a function to the function unitarily composed with another
+ * function
+ * 
+ * @author Stephen Crowley ©2024-2025
+ * @see arb.documentation.BusinessSourceLicenseVersionOnePointOne © terms
+ */
+public class AbsoluteUnitaryTimeChange implements
+                                       RealFunction
+{
+
+  final Context context = new Context();
+
+  public AbsoluteUnitaryTimeChange(RealFunction f, RealFunction ψ)
+  {
+    super();
+
+    context.registerFunction("f", f);
+    context.registerFunction("ψ", ψ);
+    u     = RealFunction.parse("U:t->sqrt(|diff(ψ(t),t)|)*f(ψ(t))", context);
+    Ufunc = u.instantiate();
+  }
+
+  final RealFunction                           Ufunc;
+  private Expression<Real, Real, RealFunction> u;
+
+  @Override
+  public Real evaluate(Real t, int order, int bits, Real res)
+  {
+    return Ufunc.evaluate(t, order, bits, res);
+  }
+
+}
--- src/test/java/arb/operators/AbsoluteUnitaryTimeChangeTest.java
+++ src/test/java/arb/operators/AbsoluteUnitaryTimeChangeTest.java
@@ -3,7 +3,15 @@ package arb.operators;
 import arb.functions.real.RealFunction;
 import junit.framework.TestCase;
 
-public class UnitaryTimeChangeTest extends
+/**
+ * TODO: make an Operator which would be a Function<RealFunction,RealFunction>
+ * which would map a function to the function unitarily composed with another
+ * function
+ * 
+ * @author Stephen Crowley ©2024-2025
+ * @see arb.documentation.BusinessSourceLicenseVersionOnePointOne © terms
+ */
+public class AbsoluteUnitaryTimeChangeTest extends
                                    TestCase
 {
 
@@ -12,7 +20,7 @@ public class UnitaryTimeChangeTest extends
     RealFunction      f = RealFunction.express("tanh(x)");
     RealFunction      ψ = RealFunction.express("x^2");
     RealFunction      g = RealFunction.express("sqrt(diff(t^2,t))*tanh(t^2)");
-    try ( UnitaryTimeChange U = new UnitaryTimeChange(f,
+    try ( AbsoluteUnitaryTimeChange U = new AbsoluteUnitaryTimeChange(f,
                                                 ψ))
     {
       assertEquals(g.eval(2.3), U.eval(2.3));
