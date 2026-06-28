8f5f326ff00a9671ca4900f841f897e43989bb3d
Stephen Crowley <stephencrowley214@gmail.com>
2026-06-28 22:25:13 +0700

    ...

--- debian/bug-presubj.ex
+++
@@ -1,3 +0,0 @@
-# Installed as usr/share/bug/arb4j/presubj in arb4j
-# See dh_bugfiles(1) and /usr/share/doc/reportbug/README.developers.gz
-# The contents of this file are displayed to the user by the bug reporting tool.
